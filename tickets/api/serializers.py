de6f3357a5615dd6380b16136249a291402ed78c
mrndhlovu <mrndhlovu@gmail.com>
2019-06-30 22:43:57 +0000

    add ticket app, added serialisers

--- tickets/api/serializers.py
+++ tickets/api/serializers.py
@@ -4,4 +4,4 @@ from tickets.models import Ticket
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
-        fileds = ('title', 'ticket_id', 'description', 'created_at', 'status', 'priority_level')
+        fields = '__all__'
--- unicorn/urls.py
+++ unicorn/urls.py
@@ -21,7 +21,7 @@ from django.views.generic import TemplateView
 urlpatterns = [ 
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    re_path('.*', TemplateView.as_view(template_name='index.html')),
+    # re_path('.*', TemplateView.as_view(template_name='index.html')),
     path('api/', include('tickets.api.urls'))
   
 ]
