350ed6109ca03294a2195231fdd925dfecb67eb9
mrndhlovu <mrndhlovu@gmail.com>
2019-06-30 08:19:50 +0000

    Install new react app using yarn

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import UnicornLayout from './containers/UnicornLayout'
 
-
-const App = () => {
+function App() {
   return (
-    <UnicornLayout>
-       Content
-    </UnicornLayout>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
