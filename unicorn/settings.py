bfa22b73a702b99396e14647fe476ee3d0b91d51
mrndhlovu <mrndhlovu@gmail.com>
2019-06-30 00:51:43 +0000

    Hook frontend url to backend routes

--- unicorn/settings.py
+++ unicorn/settings.py
@@ -245,7 +245,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'build/static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
