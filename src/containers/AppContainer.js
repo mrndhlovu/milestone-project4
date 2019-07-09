18f687e9e8d49e0b96d13267c8f29fdccf19dcf8
mndhlovu <mndhlovu@hubspot.com>
2019-07-10 00:52:47 +0100

    Ready to start development  but app has build errors when delopying to heroku

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import {
   Container,
   Divider,
@@ -10,108 +11,112 @@ import {
   Segment
 } from "semantic-ui-react";
 
-const AppContainer = () => (
-  <div>
-    <Menu fixed="top" inverted>
-      <Container>
-        <Menu.Item as="a" header>
-          {/* <Image size="mini" src="/logo.png" style={{ marginRight: "1.5em" }} /> */}
-          Unicorn
-        </Menu.Item>
-        <Menu.Item as="a">Home</Menu.Item>
+class AppContainer extends Component {
+  render() {
+    return (
+      <div>
+        <Menu fixed="top" inverted>
+          <Container>
+            <Menu.Item as="a" header>
+              {/* <Image size="mini" src="/logo.png" style={{ marginRight: "1.5em" }} /> */}
+              Unicorn
+            </Menu.Item>
+            <Menu.Item as="a">Home</Menu.Item>
 
-        <Dropdown item simple text="Dropdown">
-          <Dropdown.Menu>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Header>Header Item</Dropdown.Header>
-            <Dropdown.Item>
-              <i className="dropdown icon" />
-              <span className="text">Submenu</span>
+            <Dropdown item simple text="Dropdown">
               <Dropdown.Menu>
+                <Dropdown.Item>
+                  <Link to="signup">Signup</Link>
+                </Dropdown.Item>
                 <Dropdown.Item>List Item</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Header>Header Item</Dropdown.Header>
+                <Dropdown.Item>
+                  <i className="dropdown icon" />
+                  <span className="text">Submenu</span>
+                  <Dropdown.Menu>
+                    <Dropdown.Item>
+                      <Link to="login">Login</Link>
+                    </Dropdown.Item>
+                    <Dropdown.Item>List Item</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown.Item>
                 <Dropdown.Item>List Item</Dropdown.Item>
               </Dropdown.Menu>
-            </Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Container>
-    </Menu>
+            </Dropdown>
+          </Container>
+        </Menu>
 
-    <Container text style={{ marginTop: "7em" }}>
-      <Header as="h1">Semantic UI React Fixed Template</Header>
-      <p>This is a basic fixed menu template using fixed size containers.</p>
-      <p>
-        A text container is used for the main container, which is useful for
-        single column layouts.
-      </p>
-      {PaymentResponse.children}
-    </Container>
+        <Container text style={{ marginTop: "7em" }}>
+          <Header as="h1">Semantic UI React Fixed Template</Header>
 
-    <Segment
-      inverted
-      vertical
-      style={{ margin: "5em 0em 0em", padding: "5em 0em" }}
-    >
-      <Container textAlign="center">
-        <Grid divided inverted stackable>
-          <Grid.Column width={3}>
-            <Header inverted as="h4" content="Group 1" />
-            <List link inverted>
-              <List.Item as="a">Link One</List.Item>
-              <List.Item as="a">Link Two</List.Item>
-              <List.Item as="a">Link Three</List.Item>
-              <List.Item as="a">Link Four</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Header inverted as="h4" content="Group 2" />
-            <List link inverted>
-              <List.Item as="a">Link One</List.Item>
-              <List.Item as="a">Link Two</List.Item>
-              <List.Item as="a">Link Three</List.Item>
-              <List.Item as="a">Link Four</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Header inverted as="h4" content="Group 3" />
-            <List link inverted>
-              <List.Item as="a">Link One</List.Item>
-              <List.Item as="a">Link Two</List.Item>
-              <List.Item as="a">Link Three</List.Item>
-              <List.Item as="a">Link Four</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={7}>
-            <Header inverted as="h4" content="Footer Header" />
-            <p>
-              Extra space for a call to action inside the footer that could help
-              re-engage users.
-            </p>
-          </Grid.Column>
-        </Grid>
+          {this.props.children}
+        </Container>
 
-        <Divider inverted section />
-        {/* <Image centered size="mini" src="/logo.png" /> */}
-        <List horizontal inverted divided link size="small">
-          <List.Item as="a" href="#">
-            Site Map
-          </List.Item>
-          <List.Item as="a" href="#">
-            Contact Us
-          </List.Item>
-          <List.Item as="a" href="#">
-            Terms and Conditions
-          </List.Item>
-          <List.Item as="a" href="#">
-            Privacy Policy
-          </List.Item>
-        </List>
-      </Container>
-    </Segment>
-  </div>
-);
+        <Segment
+          inverted
+          vertical
+          style={{ margin: "5em 0em 0em", padding: "5em 0em" }}
+        >
+          <Container textAlign="center">
+            <Grid divided inverted stackable>
+              <Grid.Column width={3}>
+                <Header inverted as="h4" content="Group 1" />
+                <List link inverted>
+                  <List.Item as="a">Link One</List.Item>
+                  <List.Item as="a">Link Two</List.Item>
+                  <List.Item as="a">Link Three</List.Item>
+                  <List.Item as="a">Link Four</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Header inverted as="h4" content="Group 2" />
+                <List link inverted>
+                  <List.Item as="a">Link One</List.Item>
+                  <List.Item as="a">Link Two</List.Item>
+                  <List.Item as="a">Link Three</List.Item>
+                  <List.Item as="a">Link Four</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Header inverted as="h4" content="Group 3" />
+                <List link inverted>
+                  <List.Item as="a">Link One</List.Item>
+                  <List.Item as="a">Link Two</List.Item>
+                  <List.Item as="a">Link Three</List.Item>
+                  <List.Item as="a">Link Four</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={7}>
+                <Header inverted as="h4" content="Footer Header" />
+                <p>
+                  Extra space for a call to action inside the footer that could
+                  help re-engage users.
+                </p>
+              </Grid.Column>
+            </Grid>
+
+            <Divider inverted section />
+            {/* <Image centered size="mini" src="/logo.png" /> */}
+            <List horizontal inverted divided link size="small">
+              <List.Item as="a" href="#">
+                Site Map
+              </List.Item>
+              <List.Item as="a" href="#">
+                Contact Us
+              </List.Item>
+              <List.Item as="a" href="#">
+                Terms and Conditions
+              </List.Item>
+              <List.Item as="a" href="#">
+                Privacy Policy
+              </List.Item>
+            </List>
+          </Container>
+        </Segment>
+      </div>
+    );
+  }
+}
 
 export default AppContainer;
