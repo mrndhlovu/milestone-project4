7f7b03280f23599df597dda6f017403356207e2c
mndhlovu <mndhlovu@hubspot.com>
2019-07-09 01:11:43 +0100

    Semantic react ui setup
    React app with semintic react ui installed

---
+++ frontend/unicorn-react/src/Routes.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+import { Route } from "react-router-dom";
+import Tickets from "./js/components/tickets/TicketsList";
+import Signup from "./js/components/auth/SignupModal";
+import Home from "./js/components/home/Home";
+
+const BaseRouter = () => {
+  return (
+    <Fragment>
+      <Route exact path="/" component={Home} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/tickets" component={Tickets} />
+      {/* <Route path="/features" component={Features} />
+      <Route path="/pricing" component={Pricing} /> */}
+    </Fragment>
+  );
+};
+
+export default BaseRouter;
