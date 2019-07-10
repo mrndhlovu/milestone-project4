6b041fea6bbd159db223e12e7910f22c73607616
mndhlovu <mndhlovu@hubspot.com>
2019-07-10 18:06:22 +0100

    Added login and signup forms

--- src/components/userAuth/Login.js
+++ src/components/userAuth/Login.js
@@ -1,9 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment
+} from "semantic-ui-react";
 
-export class Login extends Component {
-  render() {
-    return <div>Login</div>;
-  }
-}
+const Login = () => (
+  <Grid textAlign="center" style={{ height: "50vh" }} verticalAlign="middle">
+    <Grid.Column style={{ maxWidth: 450 }}>
+      <Header as="h2" color="black" textAlign="center">
+        <Image src="/logo.png" />
+        Log-in to your account
+      </Header>
+      <Form size="large">
+        <Segment stacked>
+          <Form.Input
+            fluid
+            icon="user"
+            iconPosition="left"
+            placeholder="Username"
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            placeholder="Password"
+            type="password"
+          />
+
+          <Button color="teal" fluid size="large">
+            Login
+          </Button>
+        </Segment>
+      </Form>
+      <Message>
+        New to us? <Link to="/signup">Sign Up</Link>
+      </Message>
+    </Grid.Column>
+  </Grid>
+);
 
 export default Login;
--- src/components/userAuth/Signup.js
+++ src/components/userAuth/Signup.js
@@ -1,9 +1,73 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment
+} from "semantic-ui-react";
 
-export class Signup extends Component {
-  render() {
-    return <div>Signup</div>;
-  }
-}
+const Signup = () => (
+  <Grid textAlign="center" style={{ height: "50vh" }} verticalAlign="middle">
+    <Grid.Column style={{ maxWidth: 450 }}>
+      <Header as="h2" color="black" textAlign="center">
+        <Image src="/logo.png" />
+        Create an account
+      </Header>
+      <Form size="large">
+        <Segment stacked>
+          <Form.Input
+            fluid
+            icon="user"
+            iconPosition="left"
+            placeholder="First Name"
+          />
+          <Form.Input
+            fluid
+            icon="user"
+            iconPosition="left"
+            placeholder="Last Name"
+          />
+          <Form.Input
+            fluid
+            icon="user"
+            iconPosition="left"
+            placeholder="Username"
+          />
+          <Form.Input
+            fluid
+            icon="user"
+            iconPosition="left"
+            placeholder="E-mail address"
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            placeholder="Password"
+            type="password1"
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            placeholder="Confirm password"
+            type="password2"
+          />
+
+          <Button color="teal" fluid size="large">
+            Signup
+          </Button>
+        </Segment>
+      </Form>
+      <Message>
+        Already have an account? <Link to="/login">Login</Link>
+      </Message>
+    </Grid.Column>
+  </Grid>
+);
 
 export default Signup;
