6cc9ceb7f25620190e7e5789cd2827d841715693
mndhlovu <mndhlovu@hubspot.com>
2019-07-14 01:24:22 +0100

    Updated django

--- unicorn/settings.py
+++ unicorn/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '127.0.0.1', 'https://the-unicorn-attractor.herokuapp.com/']
+    'https://the-unicorn-attractor.herokuapp.com/', 'localhost', '127.0.0.1']
 
 
 # Application definition
