d9f70615cec89f7389fab21f3979a536552a4ac3
mndhlovu <mndhlovu@hubspot.com>
2019-07-20 11:47:17 +0100

    Deploy to heroku

--- unicorn/settings.py
+++ unicorn/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://the-unicorn-attractor.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'the-unicorn-attractor.herokuapp.com/']
 
 
 # Application definition
@@ -146,11 +146,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'build/static')
-# ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_STORAGE = 'whitenoise.django.GzipmanifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 REST_FRAMEWORK = {
