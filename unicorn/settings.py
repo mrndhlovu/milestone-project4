e3ce8d7291c3f0dd0f0cc8ad0afc799232864697
mndhlovu <mndhlovu@hubspot.com>
2019-07-20 21:07:02 +0100

    set debug value

--- unicorn/settings.py
+++ unicorn/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = (os.environ.get('DEGUB_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'https://the-unicorn-attractor.herokuapp.com/']
-DEBUG = (os.environ.get('DEGUB_VALUE') == 'True')
+
 
 # Application definition
 
