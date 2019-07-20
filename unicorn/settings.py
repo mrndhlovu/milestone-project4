fb8151f53b75fc4d18de455c07597cfa9f636c07
mndhlovu <mndhlovu@hubspot.com>
2019-07-20 20:33:14 +0100

    Added django-heroku

--- unicorn/settings.py
+++ unicorn/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -160,6 +161,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+django_heroku.settings(locals())
 
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
