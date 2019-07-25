80b891b3d69298753b55315ef3a3769fbe884d3e
mndhlovu <mndhlovu@hubspot.com>
2019-07-25 19:11:10 +0100

    Added knox authentication

--- unicorn/settings.py
+++ unicorn/settings.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'corsheaders'
+    'corsheaders',
+    'knox'
 
 ]
 
@@ -159,7 +160,8 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication')
 }
 
 django_heroku.settings(locals())
