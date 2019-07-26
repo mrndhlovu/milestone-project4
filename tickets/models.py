ffeb9e98d007383f4e820892ccc255e10a6e9826
mndhlovu <mndhlovu@hubspot.com>
2019-07-26 10:36:52 +0100

    Changed ticket model ondelete cascade to setnull

--- tickets/models.py
+++ tickets/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
+# Create ticket model
 class Ticket(models.Model):
     subject = models.CharField(max_length=120)
     description = models.TextField()
@@ -12,7 +12,7 @@ class Ticket(models.Model):
     title = models.CharField(max_length=120)
     tags = models.CharField(max_length=50, blank=True)
     owner = models.ForeignKey(
-        User, related_name="tickets", on_delete=models.CASCADE, null=True)
+        User, related_name="tickets", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
