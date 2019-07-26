2078e25a1979856731742d469b072c36794d71e2
mndhlovu <mndhlovu@hubspot.com>
2019-07-26 16:58:25 +0100

    Fixed serilizer typo and added index 1
    was getting error :
    Object of type 'AuthToken' is not JSON serializable
    
    The Token.objects.create returns a tuple (instance, token). So in order to get token i used the index 1

--- accounts/api/api.py
+++ accounts/api/api.py
@@ -6,26 +6,24 @@ from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
 
 # Create registration api
 class RegistrationAPI(generics.GenericAPIView):
-    serilizer_class = RegisterSerializer
+    serializer_class = RegisterSerializer
 
     def post(self, request, *args, **kwargs):
-        serilizer = self.get_serializer(data=request.data)
-        serilizer.is_valid(raise_exception=True)
-        user = serilizer.save()
-        return Response({'user': UserSerializer(user, context=self.get_serializer_context()).data, 'token':
-                         AuthToken.objects.create(user)})
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({"user": UserSerializer(user, context=self.get_serializer_context()).data, "token": AuthToken.objects.create(user)[1]})
 
 
 # Create login api
 class LoginAPI(generics.GenericAPIView):
-    serilizer_class = LoginSerializer
+    serializer_class = LoginSerializer
 
     def post(self, request, *args, **kwargs):
-        serilizer = self.get_serializer(data=request.data)
-        serilizer.is_valid(raise_exception=True)
-        user = serilizer.validated_data
-        return Response({'user': UserSerializer(user, context=self.get_serializer_context()).data, 'token':
-                         AuthToken.objects.create(user)})
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({"user": UserSerializer(user, context=self.get_serializer_context()).data, "token": AuthToken.objects.create(user)[1]})
 
 
 # Create user api
@@ -33,7 +31,7 @@ class UserAPI(generics.RetrieveAPIView):
     permissions._classes = [
         permissions.IsAuthenticated,
     ]
-    serilizer_class = UserSerializer
+    serializer_class = UserSerializer
 
     def get_object(self):
         return self.request.user
