fd5ff313e69eef2190f4d3a4efebbb12dd1d625e
mndhlovu <mndhlovu@hubspot.com>
2019-07-27 22:38:26 +0100

    Changed base url string

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,4 +1,4 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://the-unicorn-attractor.herokuapp.com/"
+    ? "https://the-unicorn-attractor.herokuapp.com"
     : process.env.REACT_APP_DEV_API_URL;
