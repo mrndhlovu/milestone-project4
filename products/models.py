2b43aa651b89370b9f504d510768235849f8cbf8
mndhlovu <mndhlovu@hubspot.com>
2019-07-31 14:01:31 +0100

    Added Products app model

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=254, default='')
+    name = models.CharField(max_length=100, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
