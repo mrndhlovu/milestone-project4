e59d253a1d4e69a8dc0deef758a3c1eec229a706
mndhlovu <mndhlovu@hubspot.com>
2019-08-05 20:12:46 +0100

    Setup grid layout of checkout.js file

--- src/components/ecommerce/Checkout.js
+++ src/components/ecommerce/Checkout.js
@@ -1,8 +1,41 @@
 import React, { Component } from "react";
 
+import styled from "styled-components";
+
+import BillingInformation from "./BillingInformation";
+import PaymentDetails from "./PaymentDetails";
+import OrderSummary from "./OrderSummary";
+
+import { Grid, Container } from "semantic-ui-react";
+
+const StyledContainer = styled(Container)`
+  padding-top: 1.5rem;
+`;
+
 export class Checkout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: ""
+    };
+  }
   render() {
-    return <div>Checkout</div>;
+    return (
+      <StyledContainer>
+        <Grid>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <BillingInformation />
+            </Grid.Column>
+
+            <Grid.Column>
+              <OrderSummary />
+              <PaymentDetails />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </StyledContainer>
+    );
   }
 }
 
