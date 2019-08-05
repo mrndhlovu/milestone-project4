41d0e2d7e021b0d69a50990346ef247b6b490fc0
mndhlovu <mndhlovu@hubspot.com>
2019-08-05 21:08:10 +0100

    Added discount coupon input

--- src/components/ecommerce/OrderSummary.js
+++ src/components/ecommerce/OrderSummary.js
@@ -1,22 +1,72 @@
 import React, { Component } from "react";
 
-import { Card } from "semantic-ui-react";
-
-const description = [
-  "Amy is a violinist with 2 years experience in the wedding industry.",
-  "She enjoys the outdoors and currently resides in upstate New York."
-].join(" ");
+import { Card, Input, List, Button } from "semantic-ui-react";
 
 export class OrderSummary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemPrice: "",
+      itemName: "",
+      itemDesription: "",
+      showCouponInput: ""
+    };
+    this.handleHasCouponClick = this.handleHasCouponClick.bind(this);
+    this.renderCouponInput = this.renderCouponInput.bind(this);
+  }
+
+  handleHasCouponClick() {
+    this.setState({ showCouponInput: true });
+  }
+
+  renderCouponInput() {
+    return (
+      <Card.Content>
+        <Input
+          action={{
+            color: "teal",
+            labelPosition: "left",
+            icon: "cart",
+            content: "Coupon number"
+          }}
+          actionPosition="left"
+          placeholder="e.g: DISCOUNT10"
+        />
+      </Card.Content>
+    );
+  }
+  renderCartItem() {
+    const { itemPrice, itemDesription, itemName } = this.state;
+    return (
+      <Card.Content>
+        <Card.Header content={`Order Item Name: ${itemName}`} />
+        <Card.Meta content={`Order Item:  ${itemDesription}`} />
+        <Card.Description content={`Price: ${itemPrice}`} />
+      </Card.Content>
+    );
+  }
+
   render() {
+    const { showCouponInput } = this.state;
     return (
       <div>
         <Card fluid>
           <Card.Content header="Order Summary" />
-          <Card.Content description={description} />
+          <Card.Content>{this.renderCartItem()}</Card.Content>
+
           <Card.Content extra>
-            Have a coupon? Click here to enter your code.
+            <List.Content>
+              Have a coupon?
+              <Button
+                content="Standard"
+                basic
+                onClick={this.handleHasCouponClick}
+              >
+                Click here to enter your code.
+              </Button>
+            </List.Content>
           </Card.Content>
+          {showCouponInput ? this.renderCouponInput() : null}
         </Card>
       </div>
     );
