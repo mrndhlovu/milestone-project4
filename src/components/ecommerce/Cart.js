e8c81b2e3d0e8af2df448b114ec0336a1ca99739
mndhlovu <mndhlovu@hubspot.com>
2019-08-05 23:44:00 +0100

    Added Cart page layout

--- src/components/ecommerce/Cart.js
+++ src/components/ecommerce/Cart.js
@@ -1,8 +1,41 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+import styled from "styled-components";
+
+import { Container } from "semantic-ui-react";
+
+import { Card, Button } from "semantic-ui-react";
+
+export const StyledContainer = styled(Container)`
+  padding-top: 1.5rem;
+`;
 
 export class Cart extends Component {
   render() {
-    return <div>Cart</div>;
+    return (
+      <div>
+        <StyledContainer>
+          <Card fluid>
+            <Card.Content header="Your Cart" />
+            <Card.Content description="description" />
+            <Card.Content extra>
+              <Button
+                floated="right"
+                color="orange"
+                as={NavLink}
+                to="/checkout"
+              >
+                Checkout
+              </Button>
+              <Button floated="right" color="orange">
+                Update
+              </Button>
+            </Card.Content>
+          </Card>
+        </StyledContainer>
+      </div>
+    );
   }
 }
 
