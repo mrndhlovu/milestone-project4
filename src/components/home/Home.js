07e2ef8a7f23f8cfbacae27cf997959d1bef85bb
mndhlovu <mndhlovu@hubspot.com>
2019-08-08 22:53:53 +0100

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 import {
   Button,
   Container,
-  Divider,
   Grid,
   Header,
   Image,
@@ -14,6 +15,13 @@ import {
 import { logOut } from "../../actions/AuthActions";
 
 import HeadingImage from "./HeadingImage";
+import image_1 from "../../images/image_1.jpg";
+import image_2 from "../../images/image_2.jpg";
+
+const StyledHeaderSpan = styled.span`
+  text-transform: uppercase;
+  text-justify: center !important;
+`;
 
 export class Home extends Component {
   constructor(props) {
@@ -43,38 +51,35 @@ export class Home extends Component {
           data={{ headerText, headerButtonUrl, headerButtonText, subHeading }}
         />
 
-        <Segment style={{ padding: "8em 0em" }} vertical>
+        <Segment style={{ padding: "4em 0em" }} vertical>
           <Grid container stackable verticalAlign="middle">
             <Grid.Row>
               <Grid.Column width={8}>
-                <Header as="h3" style={{ fontSize: "2em" }}>
-                  We Help Companies and Companions
+                <Header as="h3">
+                  <StyledHeaderSpan>
+                    Problems are everywhere, solutions are here!
+                  </StyledHeaderSpan>
                 </Header>
-                <p style={{ fontSize: "1.33em" }}>
-                  We can give your company superpowers to do things that they
-                  never thought possible. Let us delight your customers and
-                  empower your needs... through pure data analytics.
+                <p style={{ fontSize: "1.2rem" }}>
+                  We've got you covered for with all python, react, django,
+                  flask, code issues.
                 </p>
-                <Header as="h3" style={{ fontSize: "2em" }}>
-                  We Make Bananas That Can Dance
+                <Header as="h3">
+                  <StyledHeaderSpan>Explore Unicorn-Pro </StyledHeaderSpan>
                 </Header>
-                <p style={{ fontSize: "1.33em" }}>
-                  Yes that's right, you thought it was the stuff of dreams, but
-                  even bananas can be bioengineered.
+                <p style={{ fontSize: "1.2rem" }}>
+                  Get your questions or issues resolved in no time
                 </p>
               </Grid.Column>
               <Grid.Column floated="right" width={6}>
-                <Image
-                  bordered
-                  rounded
-                  size="large"
-                  src="/images/wireframe/white-image.png"
-                />
+                <Image bordered rounded src={image_1} />
               </Grid.Column>
             </Grid.Row>
             <Grid.Row>
               <Grid.Column textAlign="center">
-                <Button size="huge">Check Them Out</Button>
+                <Button size="large" color="orange" as={NavLink} to="/pricing">
+                  <StyledHeaderSpan>Explore Pro Service</StyledHeaderSpan>
+                </Button>
               </Grid.Column>
             </Grid.Row>
           </Grid>
@@ -84,61 +89,45 @@ export class Home extends Component {
           <Grid celled="internally" columns="equal" stackable>
             <Grid.Row textAlign="center">
               <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-                <Header as="h3" style={{ fontSize: "2em" }}>
-                  "What a Company"
+                <Header as="h3">
+                  <StyledHeaderSpan>File your ticket</StyledHeaderSpan>
                 </Header>
                 <p style={{ fontSize: "1.33em" }}>
-                  That is what they all say about us
+                  Head start on coding issues and save hours!
+                </p>
+                <Header as="h3">
+                  <StyledHeaderSpan>
+                    Learn about the Unicorn Project{" "}
+                  </StyledHeaderSpan>
+                </Header>
+                <p style={{ fontSize: "1.33em" }}>
+                  Learn more on how it is structure and how it runs
                 </p>
               </Grid.Column>
               <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-                <Header as="h3" style={{ fontSize: "2em" }}>
-                  "I shouldn't have gone with their competitor."
-                </Header>
-                <p style={{ fontSize: "1.33em" }}>
-                  <Image avatar src="/images/avatar/large/nan.jpg" />
-                  <b>Nan</b> Chief Fun Officer Acme Toys
-                </p>
+                <Image src={image_2} />
               </Grid.Column>
             </Grid.Row>
           </Grid>
         </Segment>
 
-        <Segment style={{ padding: "8em 0em" }} vertical>
+        <Segment style={{ padding: "3em 0em" }} vertical>
           <Container text>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              Breaking The Grid, Grabs Your Attention
+            <Header as="h3">
+              <StyledHeaderSpan> Make a kind contribution</StyledHeaderSpan>
             </Header>
             <p style={{ fontSize: "1.33em" }}>
-              Instead of focusing on content creation and hard work, we have
-              learned how to master the art of doing nothing by providing
-              massive amounts of whitespace and generic content that can seem
-              massive, monolithic and worth your attention.
+              To keep the business run and the lights on, you can make a kind
+              contribution.
             </p>
-            <Button as="a" size="large">
-              Read More
-            </Button>
-
-            <Divider
-              as="h4"
-              className="header"
-              horizontal
-              style={{ margin: "3em 0em", textTransform: "uppercase" }}
+            <Button
+              as={NavLink}
+              to="/checkout"
+              color="orange"
+              as="a"
+              size="medium"
             >
-              Case Studies
-            </Divider>
-
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              Did We Tell You About Our Bananas?
-            </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              Yes I know you probably disregarded the earlier boasts as
-              non-sequitur filler content, but it's really true. It took years
-              of gene splicing and combinatory DNA research, but our bananas can
-              really dance.
-            </p>
-            <Button as="a" size="large">
-              I'm Still Quite Interested
+              Make a Donation
             </Button>
           </Container>
         </Segment>
