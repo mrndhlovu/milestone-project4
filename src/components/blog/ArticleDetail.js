3ec481d6145e0091c114b314ba64daa53825069e
mndhlovu <mndhlovu@hubspot.com>
2019-08-08 21:24:25 +0100

    Create ArticleDetail.js

---
+++ src/components/blog/ArticleDetail.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class ArticleDetail extends Component {
+  render() {
+    return <div>ArticleDetail</div>;
+  }
+}
+
+export default ArticleDetail;
