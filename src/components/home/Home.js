6b52cf037264469dc97d786c3ef261beb943a4ec
mndhlovu <mndhlovu@hubspot.com>
2019-08-08 23:07:13 +0100

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -20,7 +20,6 @@ import image_2 from "../../images/image_2.jpg";
 
 const StyledHeaderSpan = styled.span`
   text-transform: uppercase;
-  text-justify: center !important;
 `;
 
 export class Home extends Component {
@@ -50,10 +49,12 @@ export class Home extends Component {
         <HeadingImage
           data={{ headerText, headerButtonUrl, headerButtonText, subHeading }}
         />
-
-        <Segment style={{ padding: "4em 0em" }} vertical>
-          <Grid container stackable verticalAlign="middle">
+        <Segment style={{ padding: "3rem 0rem" }} vertical>
+          <Grid container stackable verticalAlign="middle" textAlign="center">
             <Grid.Row>
+              <Grid.Column floated="left" textAlign="center" width={8}>
+                <Image bordered rounded src={image_1} />
+              </Grid.Column>
               <Grid.Column width={8}>
                 <Header as="h3">
                   <StyledHeaderSpan>
@@ -71,13 +72,10 @@ export class Home extends Component {
                   Get your questions or issues resolved in no time
                 </p>
               </Grid.Column>
-              <Grid.Column floated="right" width={6}>
-                <Image bordered rounded src={image_1} />
-              </Grid.Column>
             </Grid.Row>
             <Grid.Row>
               <Grid.Column textAlign="center">
-                <Button size="large" color="orange" as={NavLink} to="/pricing">
+                <Button size="medium" color="orange" as={NavLink} to="/pricing">
                   <StyledHeaderSpan>Explore Pro Service</StyledHeaderSpan>
                 </Button>
               </Grid.Column>
@@ -85,48 +83,48 @@ export class Home extends Component {
           </Grid>
         </Segment>
 
-        <Segment style={{ padding: "0em" }} vertical>
+        <Segment style={{ padding: "0rem" }} vertical>
           <Grid celled="internally" columns="equal" stackable>
             <Grid.Row textAlign="center">
-              <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-                <Header as="h3">
+              <Grid.Column
+                style={{ paddingBottom: "5rem", paddingTop: "5rem" }}
+              >
+                <Header as="h2">
                   <StyledHeaderSpan>File your ticket</StyledHeaderSpan>
                 </Header>
-                <p style={{ fontSize: "1.33em" }}>
+                <p style={{ fontSize: "1.3rem" }}>
                   Head start on coding issues and save hours!
                 </p>
-                <Header as="h3">
+                <Header as="h2">
                   <StyledHeaderSpan>
                     Learn about the Unicorn Project{" "}
                   </StyledHeaderSpan>
                 </Header>
-                <p style={{ fontSize: "1.33em" }}>
-                  Learn more on how it is structure and how it runs
+                <p style={{ fontSize: "1.3rem" }}>
+                  Learn more on how it is structure and how it runs, we tell it
+                  all
                 </p>
               </Grid.Column>
-              <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-                <Image src={image_2} />
+              <Grid.Column
+                style={{ paddingBottom: "3rem", paddingTop: "2em" }}
+                width={8}
+              >
+                <Image bordered rounded src={image_2} />
               </Grid.Column>
             </Grid.Row>
           </Grid>
         </Segment>
 
-        <Segment style={{ padding: "3em 0em" }} vertical>
+        <Segment style={{ padding: "3rem 0rem" }} textAlign="center" vertical>
           <Container text>
-            <Header as="h3">
+            <Header as="h2">
               <StyledHeaderSpan> Make a kind contribution</StyledHeaderSpan>
             </Header>
-            <p style={{ fontSize: "1.33em" }}>
+            <p style={{ fontSize: "1.3rem" }}>
               To keep the business run and the lights on, you can make a kind
               contribution.
             </p>
-            <Button
-              as={NavLink}
-              to="/checkout"
-              color="orange"
-              as="a"
-              size="medium"
-            >
+            <Button as={NavLink} to="/checkout" color="orange" size="medium">
               Make a Donation
             </Button>
           </Container>
