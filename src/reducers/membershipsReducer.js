02deb9ba1b758951490d4e91991cffdedf3fa868
mndhlovu <mndhlovu@hubspot.com>
2019-08-14 23:03:35 +0100

    Create membershipsReducer.js

---
+++ src/reducers/membershipsReducer.js
@@ -0,0 +1,42 @@
+import {
+  RECEIVE_MEMBERSHIPS_LIST,
+  FETCHING_DATA
+} from "../actions/ActionTypes";
+
+import { checkObjectUpdate } from "../utils/checkObjectUpdate";
+
+const initialState = {
+  membershipList: [],
+  dataReceived: false,
+  hasError: false,
+  isLoading: false
+};
+
+const fetchingData = (state, action) => {
+  return checkObjectUpdate(state, {
+    membershipList: [],
+    dataReceived: false,
+    hasError: false,
+    isLoading: true
+  });
+};
+
+const dataReceived = (state, action) => {
+  return {
+    membershipList: action.payload,
+    dataReceived: true,
+    hasError: false,
+    isLoading: false
+  };
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case FETCHING_DATA:
+      return fetchingData(state, action);
+    case RECEIVE_MEMBERSHIPS_LIST:
+      return dataReceived(state, action);
+    default:
+      return state;
+  }
+}
