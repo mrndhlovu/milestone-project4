a27a9ae07772d873d6fae091ce187c8af05dc595
mndhlovu <mndhlovu@hubspot.com>
2019-08-14 23:03:19 +0100

    Update views.py

--- tickets/api/views.py
+++ tickets/api/views.py
@@ -23,3 +23,6 @@ class TicketViewSet(viewsets.ModelViewSet):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+        
