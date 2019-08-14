dbfaad965bf55bb991b06cfdcde92c55afb6b009
mndhlovu <mndhlovu@hubspot.com>
2019-08-14 23:05:15 +0100

    Update urls.py

--- unicorn/urls.py
+++ unicorn/urls.py
@@ -4,13 +4,13 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # path('api-auth/', include('rest_framework.urls')),
-    # path('rest-auth/', include('rest_auth.urls')),
     path('admin/', admin.site.urls),
     path('tickets/', include('tickets.api.urls')),
-    # path('products/', include('products.api.urls')),
-
-    # path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('products/', include('products.api.urls')),
     path('accounts/', include('accounts.api.urls')),
-    re_path('.*', TemplateView.as_view(template_name='index.html'))
+    path('memberships/', include('memberships.api.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
+    # path('api-auth/', include('rest_framework.urls')),
+    # path('rest-auth/', include('rest_auth.urls')),
+    # path('rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
