219a8cbd937864eb7490363ba54d96c912438a8f
mndhlovu <mndhlovu@hubspot.com>
2019-08-19 20:45:12 +0100

    Update views.py

--- memberships/api/views.py
+++ memberships/api/views.py
@@ -1,9 +1,10 @@
 from memberships.models import Membership
 from .serializers import MembershipSerializer
-from rest_framework import viewsets, permissions
+from rest_framework import permissions
+from rest_framework.generics import ListAPIView
 
 
-class MembershipViewSet(viewsets.ModelViewSet):
+class MembershipListView(ListAPIView):
     serializer_class = MembershipSerializer
     queryset = Membership.objects.all()
     permission_classes = [permissions.AllowAny]
