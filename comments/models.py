27a9b7c1c0ea9e2eaa7f364fd1396e8e67854b70
mndhlovu <mndhlovu@hubspot.com>
2019-08-20 12:16:30 +0100

    Added generic relations

--- comments/models.py
+++ comments/models.py
@@ -4,16 +4,21 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
 from tickets.models import Ticket
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 User = settings.AUTH_USER_MODEL
 
 
 class Comment(models.Model):
-    ticket = models.ForeignKey(
-        Ticket, on_delete=models.CASCADE, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
     comment = models.TextField(max_length=200)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return '{}-{}'.format(self.ticket.title, str(self.user.username))
+        return str(self.user.username)
