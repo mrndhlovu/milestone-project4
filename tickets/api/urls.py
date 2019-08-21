dc851d5013311b638f52ffb3264f23a425f9fc6d
mndhlovu <mndhlovu@hubspot.com>
2019-08-21 15:45:22 +0100

    Updated backend urls handles for deleting and editing ticket

--- tickets/api/urls.py
+++ tickets/api/urls.py
@@ -1,11 +1,13 @@
 from rest_framework.routers import DefaultRouter
-from .views import TicketListView, TicketVoteToggleAPIView, TicketDetailView, CreateTicketView
+from .views import TicketListView, TicketVoteToggleAPIView, TicketDetailView, CreateTicketView, TicketUpdateView, TicketDeleteView
 from django.urls import path, re_path
 
 
 urlpatterns = [
     path('', TicketListView.as_view()),
     path('<pk>/', TicketDetailView.as_view()),
+    path('update/<pk>/', TicketUpdateView.as_view()),
+    path('delete/<pk>/', TicketDeleteView.as_view()),
     path('api/create/', CreateTicketView.as_view()),
     path('api/<id>/vote/', TicketVoteToggleAPIView.as_view()),
 ]
