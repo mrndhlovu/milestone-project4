963fde5f809c2d600c7bb9c041b9edf9608c6a68
mndhlovu <mndhlovu@hubspot.com>
2019-08-26 01:23:28 +0100

    Updated membershipd urls to include new views

--- memberships/api/urls.py
+++ memberships/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, re_path
-from .views import MembershipListView, MembershipsDetailAPI
+from .views import MembershipListView, MembershipsDetailAPI, MembershipPaymentView, MembershipTransactionUpdateView
 
+app_name = 'memberships'
 
 urlpatterns = [
-    path('', MembershipListView.as_view(), name='select'),
-    path('type/<pk>/', MembershipsDetailAPI.as_view(), name='single_option')
+    path('', MembershipListView.as_view(), name='membership_list'),
+    path('select/', MembershipListView.as_view(), name='selected_membership'),
+    path('payments/', MembershipPaymentView.as_view(), name='payments'),
+    path('update-transaction/', MembershipTransactionUpdateView.as_view(),
+         name='update_transaction/')
 
 ]
