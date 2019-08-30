603a0309502610681f7fbeaedf992cdf44ea7d40
mndhlovu <mndhlovu@hubspot.com>
2019-08-30 09:04:26 +0100

    Update models.py

--- comments/models.py
+++ comments/models.py
@@ -10,15 +10,26 @@ from django.contrib.contenttypes.models import ContentType
 User = settings.AUTH_USER_MODEL
 
 
+class CommentManger(models.Manager):
+    def filter_by_instance(self, instance):
+        content_type = ContentType.objects.get_for_model(instance.__class__)
+        obj_id = instance.id
+
+        qs = super(CommentManger, self).filter(
+            content_type=content_type, object_id=obj_id)
+        return qs
+
+
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
 
     comment = models.TextField(max_length=200)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = CommentManger()
 
     def __str__(self):
         return str(self.user.username)
