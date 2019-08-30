3654b2497443f6776eb77ccd364f059e52075a39
mndhlovu <mndhlovu@hubspot.com>
2019-08-30 08:57:14 +0100

    Added Membership cancelation and user profile api

--- memberships/api/views.py
+++ memberships/api/views.py
@@ -1,4 +1,4 @@
-from .serializers import MembershipSerializer
+from .serializers import MembershipSerializer, MembershipProfileSerializer
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_list_or_404, get_object_or_404, redirect
@@ -10,6 +10,10 @@ import json
 import stripe
 from django.contrib import messages
 from django.urls import reverse
+from django.shortcuts import get_list_or_404, get_object_or_404
+
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 def get_user_membership(request):
@@ -30,9 +34,12 @@ def get_user_subcription(request):
 
 
 def get_selected_membership(request):
-    membership_type = request['choosen_membership_type']
+
+    membership_type = request['membership_type']
+
     choosen_membership_qs = Membership.objects.filter(
         membership_type=membership_type)
+
     if choosen_membership_qs.exists():
         return choosen_membership_qs.first()
     return None
@@ -55,9 +62,9 @@ class MembershipListView(ListAPIView):
         return context
 
     def post(self, request, **kwargs):
-        data = json.loads(request.body.decode('utf-8'))
-        choosen_membership_type = data['membership_type']
+        request_data = json.loads(request.body.decode('utf-8'))
 
+        choosen_membership_type = get_selected_membership(request_data)
         user_membership = get_user_membership(request)
         user_subcription = get_user_subcription(request)
 
@@ -67,8 +74,10 @@ class MembershipListView(ListAPIView):
         if choosen_membership_qs.exists():
             choosen_membership = choosen_membership_qs.first()
 
+        if user_membership.membership == choosen_membership:
+
             if user_subcription is not None:
-                message = 'You already have this membership'
+                message = 'You already have this membership!'
                 return JsonResponse({'message': message}, status=status.HTTP_200_OK)
 
         message = {'choosen_membership_type': choosen_membership.membership_type}
@@ -84,45 +93,33 @@ class MembershipPaymentView(ListAPIView):
     ]
 
     def post(self, request, **kwargs):
-        data = json.loads(request.body.decode('utf-8'))
+        request_data = json.loads(request.body.decode('utf-8'))
 
         user_membership = get_user_membership(request)
 
         try:
-            choosen_membership = get_selected_membership(data)
-
+            choosen_membership = get_selected_membership(request_data)
         except:
             message = 'Not found'
             return JsonResponse({'message': message}, status=status.HTTP_400_BAD_REQUEST)
-
         publish_key = settings.STRIPE_PUBLISHABLE_KEY
-
         if request.method == "POST":
+
             try:
-
-                token = data['stripeToken']
-
-                # UPDATE FOR STRIPE API CHANGE 2018-05-21
-
-                '''
-                First we need to add the source for the customer
-                '''
+                token = request_data['stripeToken']
 
                 customer = stripe.Customer.retrieve(
-                    user_membership.stripe_customer_key)
+                    user_membership.stripe_customer_id)
 
-                customer.default_source = token  # 4242424242424242
+                customer.source = token
                 customer.save()
 
-                '''
-                Now we can create the subscription using only the customer as we don't need to pass their
-                credit card source anymore
-                '''
-
                 subscription = stripe.Subscription.create(
-                    customer=user_membership.stripe_customer_key,
+                    customer=user_membership.stripe_customer_id,
                     items=[
-                        {"plan": choosen_membership.stripe_plan_id},
+                        {
+                            "plan": choosen_membership.stripe_plan_id
+                        }
                     ]
                 )
 
@@ -131,8 +128,9 @@ class MembershipPaymentView(ListAPIView):
                 return JsonResponse(response_object, status=200)
 
             except:
-
-                return JsonResponse({'message': 'An error has occurred, investigate it in the console'}, status=status.HTTP_404_NOT_FOUND)
+                message = {
+                    'message': 'An error has occurred, investigate it in the console'}
+                return JsonResponse(message, status=status.HTTP_404_NOT_FOUND)
 
         context = {
             'publishKey': publish_key,
@@ -151,23 +149,23 @@ class MembershipTransactionUpdateView(ListAPIView):
 
     def post(self, request, **kwargs):
 
-        data = json.loads(request.body.decode('utf-8'))
+        request_data = json.loads(request.body.decode('utf-8'))
 
         user_membership = get_user_membership(request)
-        choosen_membership = get_selected_membership(data)
+        choosen_membership = get_selected_membership(request_data)
         user_membership.membership = choosen_membership
 
         user_membership.save()
 
-        subscription_id = data['subscription_id']
+        subscription_id = request_data['subscription_id']
 
-        sub, created = Subcription.objects.get_or_create(
-            user_membership=user_membership, is_active=True, stripe_customer_id=subscription_id)
-
-        sub.save()
+        subcription, created = Subcription.objects.get_or_create(
+            user_membership=user_membership, is_active=True)
+        subcription.stripe_subscription_id = subscription_id
+        subcription.save()
 
         context = {
-            'data': data,
+            'data': request_data,
             'message': 'Successfully created {} membership'.format(
                 choosen_membership)
         }
@@ -175,9 +173,58 @@ class MembershipTransactionUpdateView(ListAPIView):
         return JsonResponse(context, status=200, safe=False)
 
 
-class MembershipsDetailAPI(RetrieveAPIView):
+class MembershipTypesAPI(ListAPIView):
     permissions._classes = [
         permissions.AllowAny,
     ]
     serializer_class = MembershipSerializer
     queryset = Membership.objects.all()
+
+
+class MembershipsProfileAPI(ListAPIView):
+    permissions._classes = [
+        permissions.AllowAny,
+    ]
+    serializer_class = MembershipProfileSerializer
+    queryset = Subcription.objects.all()
+
+
+class CancelSubscriptionAPI(ListAPIView):
+    permissions._classes = [
+        permissions.AllowAny,
+    ]
+    serializer_class = MembershipSerializer
+    queryset = Subcription.objects.all()
+
+    def post(self, request, **kwargs):
+
+        try:
+            user_subcription = get_user_subcription(request)
+            if user_subcription.is_active is False:
+
+                message = 'No active subscription found!'
+                return JsonResponse({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+
+        except:
+            message = 'Error! could not cancel your subscription'
+            return JsonResponse({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+
+        subcription = stripe.Subscription.retrieve(
+            user_subcription.stripe_subscription_id)
+        subcription.delete()
+
+        user_subcription.is_active = False
+        user_subcription.delete()
+
+        free_membership = Membership.objects.filter(
+            membership_type='free').first()
+        user_membership = get_user_membership(request)
+
+        user_membership.membership = free_membership
+        user_membership.save()
+
+        context = {
+            'message': 'Deactivated you subscription!'
+        }
+
+        return JsonResponse(context, status=200, safe=False)
