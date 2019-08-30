93ca1963d81787745e6d1ecd2078490856bc7e9e
mndhlovu <mndhlovu@hubspot.com>
2019-08-30 09:04:33 +0100

    Update urls.py

--- memberships/api/urls.py
+++ memberships/api/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, re_path
-from .views import MembershipListView, MembershipsDetailAPI, MembershipPaymentView, MembershipTransactionUpdateView
+from .views import MembershipListView, MembershipPaymentView, CancelSubscriptionAPI, MembershipTransactionUpdateView, MembershipsProfileAPI, MembershipTypesAPI
 
 app_name = 'memberships'
 
 urlpatterns = [
     path('', MembershipListView.as_view(), name='membership_list'),
+    path('types/', MembershipTypesAPI.as_view(), name='membership_type'),
     path('select/', MembershipListView.as_view(), name='selected_membership'),
     path('payments/', MembershipPaymentView.as_view(), name='payments'),
+    path('user-profile/', MembershipsProfileAPI.as_view(), name='user_profile'),
+    path('cancel-subscription/', CancelSubscriptionAPI.as_view(),
+         name='cancel_subscription'),
     path('update-transaction/', MembershipTransactionUpdateView.as_view(),
          name='update_transaction/')
 
