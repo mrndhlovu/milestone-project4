78eddceb9e63466ae72bcfa9aaf4f877e27e476f
mndhlovu <mndhlovu@hubspot.com>
2019-09-01 12:08:40 +0100

    Create localStorageConstants.js

---
+++ src/constants/localStorageConstants.js
@@ -0,0 +1,7 @@
+export const STRIPE_TOKEN = localStorage.getItem("stripeToken");
+
+export const SESSION_TOKEN = localStorage.getItem("sessionToken");
+
+export const SUBCRIPTION_ID = localStorage.getItem("subscriptionId");
+
+export const MEMBERSHIP = localStorage.getItem("membership");
