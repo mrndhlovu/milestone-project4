9633ca77930506b37dfd1b1b6f5922d16c1dfc2c
mndhlovu <mndhlovu@hubspot.com>
2019-09-01 12:07:13 +0100

    Update PaymentDetails.js
    Handle card payments

--- src/components/ecommerce/PaymentDetails.js
+++ src/components/ecommerce/PaymentDetails.js
@@ -1,70 +1,62 @@
 import React, { Component } from "react";
-
-import { PaymentMethods } from "../../constants/constants";
+import { connect } from "react-redux";
 
 import { CardElement } from "react-stripe-elements";
 
-import { Form, Button, Card, Message, Segment } from "semantic-ui-react";
+import { Form, Button, Card, Segment } from "semantic-ui-react";
+
+import {
+  requestPayment,
+  requestChoosenMembership
+} from "../../actions/CheckoutActions";
 
 export class PaymentDetails extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      paymentOption: ""
+      paymentOption: "",
+      isLoading: false,
+      isDisabled: true
     };
 
-    this.handlePaymentOption = this.handlePaymentOption.bind(this);
+    this.handleTextInput = this.handleTextInput.bind(this);
   }
 
-  handlePaymentOption(event, methods) {
-    const paymentOption = event.target.innerText;
-
-    methods.map(method => {
-      return paymentOption === method.text
-        ? this.setState({ paymentOption: method.text })
-        : null;
-    });
+  handleTextInput() {
+    this.setState({ isDisabled: false });
   }
 
-  renderPaymentMethod() {
-    const { paymentOption } = this.state;
-    return paymentOption === "Stripe" ? (
-      <Form.Group widths="equal">
-        <Segment>
-          <CardElement />
-        </Segment>
-      </Form.Group>
-    ) : null;
+  componentDidMount() {
+    this.props.requestChoosenMembership();
+  }
+
+  handleSubmit() {
+    const { onSubmitClick, requestPayment } = this.props;
+
+    onSubmitClick();
+    requestPayment();
   }
 
   render() {
-    const { hasError, paymentOption } = this.state;
-    const { onSubmitClick } = this.props;
+    const { isLoading } = this.state;
 
     return (
       <Card fluid>
-        <Card.Content header="Payment Details" />
+        <Card.Content header="Payment" />
         <Card.Content>
-          <Form.Select
-            icon={paymentOption ? "stripe card" : "paypal card"}
-            fluid
-            label="Payment Method"
-            options={PaymentMethods}
-            placeholder="Select payment method"
-            onChange={event => this.handlePaymentOption(event, PaymentMethods)}
-          />
-
-          {paymentOption ? this.renderPaymentMethod() : null}
-          {hasError ? (
-            <Message
-              error
-              header="Action Forbidden"
-              content="You can only sign up for an account once with a given e-mail address."
-            />
-          ) : null}
+          <Form.Group widths="equal">
+            <Segment>
+              <CardElement />
+            </Segment>
+          </Form.Group>
         </Card.Content>
         <Card.Content extra>
-          <Button color="orange" fluid onClick={onSubmitClick}>
+          <Button
+            color="orange"
+            fluid
+            onClick={() => this.handleSubmit()}
+            loading={isLoading}
+          >
             PAY NOW
           </Button>
         </Card.Content>
@@ -73,4 +65,11 @@ export class PaymentDetails extends Component {
   }
 }
 
-export default PaymentDetails;
+const mapStateToProps = state => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  { requestPayment, requestChoosenMembership }
+)(PaymentDetails);
