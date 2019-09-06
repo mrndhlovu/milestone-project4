2666a6136bd4ee638bb6dd36201f81ef1e24f515
mndhlovu <mndhlovu@hubspot.com>
2019-09-06 23:10:32 +0100

    Added comments to ticketdetail serializer

--- tickets/api/serializers.py
+++ tickets/api/serializers.py
@@ -15,3 +15,18 @@ class TicketSerializer(serializers.ModelSerializer):
         rep['votes'] = instance.votes.count()
 
         return rep
+
+
+class TicketDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        rep = super(TicketDetailSerializer, self).to_representation(instance)
+
+        rep['username'] = instance.owner.username
+        rep['votes'] = instance.votes.count()
+        rep['comments'] = instance.comments
+
+        return rep
