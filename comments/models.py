3eb3ccb10146e05729b23ab7b88aaf6a46dbc345
mndhlovu <mndhlovu@hubspot.com>
2019-09-06 23:01:12 +0100

    Added comment ordering

--- comments/models.py
+++ comments/models.py
@@ -3,21 +3,22 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
-from tickets.models import Ticket
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 User = settings.AUTH_USER_MODEL
 
 
-class CommentManger(models.Manager):
+class CommentManager(models.Manager):
+
     def filter_by_instance(self, instance):
-        content_type = ContentType.objects.get_for_model(instance.__class__)
+        content_type = ContentType.objects.get_for_model(
+            instance.__class__)
         obj_id = instance.id
 
-        qs = super(CommentManger, self).filter(
+        queryset = super(CommentManager, self).filter(
             content_type=content_type, object_id=obj_id)
-        return qs
+        return queryset
 
 
 class Comment(models.Model):
@@ -25,11 +26,15 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
-
     comment = models.TextField(max_length=200)
     timestamp = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey(
+        'self', null=True, blank=True, on_delete=models.CASCADE, related_name='parent_comment')
 
-    objects = CommentManger()
+    objects = CommentManager()
 
     def __str__(self):
         return str(self.user.username)
+
+    class Meta:
+        ordering = ['-timestamp']
