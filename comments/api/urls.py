a46a7bb6f797f77bbe5abc6797abe6c3f6d5bfb7
mndhlovu <mndhlovu@hubspot.com>
2019-09-06 22:58:29 +0100

    Updated comments urls

--- comments/api/urls.py
+++ comments/api/urls.py
@@ -1,9 +1,10 @@
 
-from .views import CommentsListView
+from .views import CommentsListView, CreateCommentView, CreateCommentReplyView
 from django.urls import path
 
 
 urlpatterns = [
     path('', CommentsListView.as_view()),
-
+    path('create-comment/', CreateCommentView.as_view()),
+    path('create-reply/', CreateCommentReplyView.as_view()),
 ]
