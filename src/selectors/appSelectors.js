d92b64c64b2f5ba421bd58f3b2d36a8124d343fe
mndhlovu <mndhlovu@hubspot.com>
2019-09-07 17:09:14 +0100

    Update appSelectors.js

--- src/selectors/appSelectors.js
+++ src/selectors/appSelectors.js
@@ -5,8 +5,11 @@ export const getErrors = state => state.errorAlert;
 export const getMessage = state => state.messages;
 
 export const getTicketDetail = state => state.ticketDetail;
+export const getTicketUpdate = state => state.ticketUpdate;
 export const getTicketList = state => state.tickets;
 export const getTicket = state => state.ticket;
 
 export const getComments = state => state.comments;
 export const getVotes = state => state.votes;
+
+export const getMemberships = state => state.memberships.membershipList;
