657b10a7b03e5e3638fa98133e2ffbe12813f752
mndhlovu <mndhlovu@hubspot.com>
2019-09-07 15:12:55 +0100

    Create headerConstants.js

---
+++ src/constants/headerConstants.js
@@ -0,0 +1,41 @@
+import { getPageId } from "../utils/headerUtils";
+
+const HEADER_TEXT = {
+  home: {
+    headerText: "Unicorn Attractor",
+    headerButtonUrl: "/pricing",
+    headerButtonText: "Get Started",
+    subHeading: "Head start on coding issues and save hours!"
+  },
+  products: {
+    headerText: "Products",
+    headerButtonUrl: "/pricing",
+    headerButtonText: "See what suits you!",
+    subHeading: "How it will work for you?!"
+  },
+  pricing: {
+    headerText: "Pricing",
+    headerButtonUrl: "/create-ticket",
+    headerButtonText: "File a ticket",
+    subHeading: "Problems are everywhere, solutions are here!"
+  },
+  tickets: {
+    headerText: "Tickets",
+    headerButtonUrl: "/create-ticket",
+    headerButtonText: "Create a ticket",
+    subHeading: "Head start on coding issues and save hours!"
+  }
+};
+
+export const getHeaderElements = () => {
+  const pageId = getPageId();
+  let headerObject;
+
+  Object.keys(HEADER_TEXT).forEach(header => {
+    if (pageId === header) {
+      headerObject = HEADER_TEXT[header];
+    }
+  });
+
+  return headerObject;
+};
