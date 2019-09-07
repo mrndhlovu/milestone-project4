fc77d150b11e091c45f3d548d59f144f5fc682af
mndhlovu <mndhlovu@hubspot.com>
2019-09-07 15:13:11 +0100

    Update HeadingImage.js

--- src/components/home/HeadingImage.js
+++ src/components/home/HeadingImage.js
@@ -1,8 +1,10 @@
 import PropTypes from "prop-types";
-import React, { Component } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 import styled from "styled-components";
+
+import { getHeaderElements } from "../../constants/headerConstants";
 
 import { Button, Header, Icon, Segment } from "semantic-ui-react";
 
@@ -10,51 +12,46 @@ const StyledSegment = styled(Segment)`
   padding-bottom: 5rem !important;
 `;
 
-class HomepageHeading extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+export const HomepageHeading = ({ mobile }) => {
+  const {
+    headerText,
+    headerButtonText,
+    headerButtonUrl,
+    subHeading
+  } = getHeaderElements();
 
-  render() {
-    const {
-      mobile,
-      data: { headerText, headerButtonText, headerButtonUrl, subHeading }
-    } = this.props;
-
-    return (
-      <div>
-        <StyledSegment textAlign="center" inverted vertical>
-          <Header
-            as="h1"
-            content={headerText}
-            inverted
-            style={{
-              fontSize: mobile ? "2em" : "3em",
-              fontWeight: "normal",
-              marginBottom: 0,
-              marginTop: mobile ? "1em" : "1em"
-            }}
-          />
-          <Header
-            as="h2"
-            content={subHeading}
-            inverted
-            style={{
-              fontSize: mobile ? "1.5em" : "1.7em",
-              fontWeight: "normal",
-              marginTop: mobile ? "0.5em" : "1.5em"
-            }}
-          />
-          <Button primary size="large" as={NavLink} to={headerButtonUrl}>
-            {headerButtonText}
-            <Icon name="right arrow" />
-          </Button>
-        </StyledSegment>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <StyledSegment textAlign="center" inverted vertical>
+        <Header
+          as="h1"
+          content={headerText}
+          inverted
+          style={{
+            fontSize: mobile ? "2em" : "3em",
+            fontWeight: "normal",
+            marginBottom: 0,
+            marginTop: mobile ? "1em" : "1em"
+          }}
+        />
+        <Header
+          as="h2"
+          content={subHeading}
+          inverted
+          style={{
+            fontSize: mobile ? "1.5em" : "1.7em",
+            fontWeight: "normal",
+            marginTop: mobile ? "0.5em" : "1.5em"
+          }}
+        />
+        <Button primary size="large" as={NavLink} to={headerButtonUrl}>
+          {headerButtonText}
+          <Icon name="right arrow" />
+        </Button>
+      </StyledSegment>
+    </div>
+  );
+};
 
 HomepageHeading.propTypes = {
   mobile: PropTypes.bool
