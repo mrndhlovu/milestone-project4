51db611098407fed351c537e272e6ed36a31914a
mndhlovu <mndhlovu@hubspot.com>
2019-09-07 10:57:20 +0100

    Create appSelectors.js

---
+++ src/selectors/appSelectors.js
@@ -0,0 +1,11 @@
+export const getUser = state => state.auth;
+export const getUserProfile = state => state.user.user;
+
+export const getErrors = state => state.errorAlert;
+export const getMessage = state => state.messages;
+
+export const getTicketDetail = state => state.ticketDetail;
+export const getTicketList = state => state.tickets;
+
+export const getComments = state => state.comments;
+export const getVotes = state => state.votes;
