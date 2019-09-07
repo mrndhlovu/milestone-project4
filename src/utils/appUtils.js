4c0ad715a49d7c6d5c53e0fc56cc75595734ef03
mndhlovu <mndhlovu@hubspot.com>
2019-09-07 21:13:35 +0100

    added getSelectedMemberShip

--- src/utils/appUtils.js
+++ src/utils/appUtils.js
@@ -1,6 +1,19 @@
+import { MEMBERSHIP_OPTIONS } from "../constants/constants";
+
 export const getPageId = () => {
   const location = window.location.pathname.slice(1);
   const pageId = location === "" ? "home" : location;
 
   return pageId;
+};
+
+export const getSelectedMemberShip = choice => {
+  let selectedMembership;
+
+  Object.keys(MEMBERSHIP_OPTIONS).forEach(option => {
+    if (choice === MEMBERSHIP_OPTIONS[option].membership_type)
+      return (selectedMembership = MEMBERSHIP_OPTIONS[option].membership_type);
+  });
+
+  return selectedMembership;
 };
