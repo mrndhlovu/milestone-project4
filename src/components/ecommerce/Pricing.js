f97977595ec04e5b4141f2a44d26099442b7cd07
mndhlovu <mndhlovu@hubspot.com>
2019-09-07 17:08:28 +0100

    Extract membership options making it a separate component

--- src/components/ecommerce/Pricing.js
+++ src/components/ecommerce/Pricing.js
@@ -1,34 +1,18 @@
 import React, { Component, Fragment } from "react";
-import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 
 import HeadingImage from "../home/HeadingImage";
+import { getMemberships, getUser } from "../../selectors/appSelectors";
 
-import {
-  Button,
-  Container,
-  Grid,
-  Header,
-  List,
-  Segment,
-  Icon
-} from "semantic-ui-react";
-
-import {
-  unicornFreeServices,
-  unicornProServices
-} from "../../constants/constants";
+import { Container, Grid, List, Segment, Icon } from "semantic-ui-react";
 
 import { fetchMembershipsList } from "../../actions/MembershipActions";
+import MembershipOptions from "./MembershipOptions";
 
 export class Pricing extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      headerText: "Pricing",
-      headerButtonUrl: "/create-ticket",
-      headerButtonText: "File a ticket",
-      subHeading: "Problems are everywhere, solutions are here!",
       freeMembership: true
     };
     this.handleAddToCart = this.handleAddToCart.bind(this);
@@ -53,83 +37,26 @@ export class Pricing extends Component {
     this.props.fetchMembershipsList();
   }
 
-  renderMembershipOption() {
+  render() {
     const {
       authUser: { isAuthenticated }
     } = this.props;
-
     const { memberships } = this.props;
-    const type = "free";
-
-    return memberships.map(membership => {
-      const { price, id, slug } = membership;
-      const buttonText =
-        type === slug
-          ? isAuthenticated
-            ? "Your current membership"
-            : "Free Signup"
-          : isAuthenticated
-          ? "Upgrade your membership"
-          : "Unicorn Pro";
-      const buttonColor = type === slug ? "black" : "orange";
-      const redirectUrl =
-        type === slug ? (isAuthenticated ? "/pricing" : "/signup") : "/cart";
-      const header = type === slug ? `Unicorn ${slug}` : `Unicorn ${slug}`;
-
-      return (
-        <Grid.Column style={{ padding: "5em 5em 5em 1em" }} key={id}>
-          <Header
-            as="h2"
-            attached="top"
-            color={type === slug ? "black" : "purple"}
-            textAlign="center"
-            content={header.toUpperCase()}
-            subheader={
-              type === slug
-                ? `$ ${price} Per / month`
-                : `$ ${price} Per User / month`
-            }
-          />
-
-          <div>
-            <Segment>
-              <List>
-                {type === slug
-                  ? this.renderServicesList(unicornFreeServices)
-                  : this.renderServicesList(unicornProServices)}
-              </List>
-            </Segment>
-          </div>
-          <Button
-            attached="bottom"
-            content={buttonText.toUpperCase()}
-            as={NavLink}
-            to={redirectUrl}
-            color={buttonColor}
-          />
-        </Grid.Column>
-      );
-    });
-  }
-
-  render() {
-    const {
-      headerText,
-      headerButtonUrl,
-      headerButtonText,
-      subHeading
-    } = this.state;
 
     return (
       <Fragment>
-        <HeadingImage
-          data={{ headerText, headerButtonUrl, headerButtonText, subHeading }}
-        />
+        <HeadingImage />
 
         <Segment style={{ padding: "0em" }}>
           <Container>
             <Grid celled="internally" columns="equal" stackable>
-              <Grid.Row>{this.renderMembershipOption()}</Grid.Row>
+              <Grid.Row>
+                <MembershipOptions
+                  isAuthenticated={isAuthenticated}
+                  memberships={memberships}
+                  membershipChoice={this.renderServicesList}
+                />
+              </Grid.Row>
             </Grid>
           </Container>
         </Segment>
@@ -140,8 +67,8 @@ export class Pricing extends Component {
 
 const mapStateToProps = state => {
   return {
-    memberships: state.memberships.membershipList,
-    authUser: state.auth
+    memberships: getMemberships(state),
+    authUser: getUser(state)
   };
 };
 
