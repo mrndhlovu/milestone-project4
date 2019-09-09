7c4db74a758f74bbf268015de01d51fd997da160
mndhlovu <mndhlovu@hubspot.com>
2019-09-09 08:11:22 +0100

    Update localStorageConstants.js

--- src/constants/localStorageConstants.js
+++ src/constants/localStorageConstants.js
@@ -2,6 +2,8 @@ export const STRIPE_TOKEN = localStorage.getItem("stripeToken");
 
 export const SESSION_TOKEN = localStorage.getItem("sessionToken");
 
+export const SESSION_LIFE = localStorage.getItem("sessionLife");
+
 export const SUBCRIPTION_ID = localStorage.getItem("subscriptionId");
 
-export const MEMBERSHIP = localStorage.getItem("membership");
+export const MEMBERSHIP_SELECTED = localStorage.getItem("selectedMembership");
