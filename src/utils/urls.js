8f3224d27881c70c9617c34716f535b31e4c6838
mndhlovu <mndhlovu@hubspot.com>
2019-09-09 08:12:07 +0100

    Update urls.js

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,4 +1,25 @@
+import { SESSION_TOKEN } from "../constants/localStorageConstants";
+
 export const getRootUrl =
   process.env.NODE_ENV === "production"
     ? "https://the-unicorn-attractor.herokuapp.com"
     : process.env.REACT_APP_DEV_API_URL;
+
+export const authQueryParams = {
+  headers: {
+    Authorization: `Token ${SESSION_TOKEN}`,
+    "Content-Type": "application/json"
+  }
+};
+
+export const params = {
+  headers: {
+    "Content-Type": "text/plain"
+  }
+};
+
+export const queryParams = {
+  headers: {
+    "Content-Type": "application/json"
+  }
+};
