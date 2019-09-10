6eef72d80ad6a3f082d5df300ee08d6262a5e11d
mndhlovu <mndhlovu@hubspot.com>
2019-09-10 21:29:43 +0100

    Update appUtils.js

--- src/utils/appUtils.js
+++ src/utils/appUtils.js
@@ -1,4 +1,4 @@
-import { MEMBERSHIP_OPTIONS } from "../constants/constants";
+import { MEMBERSHIP_OPTIONS, USER_PROFILE } from "../constants/constants";
 
 export const getPageId = () => {
   const location = window.location.pathname.slice(1);
@@ -17,3 +17,52 @@ export const getSelectedMemberShip = choice => {
 
   return selectedMembership;
 };
+
+export const getUser = () => JSON.parse(localStorage.getItem("client"));
+
+export const isTicketOwner = id => {
+  if (getUser()) {
+    return getUser().userId === id;
+  } else {
+    return false;
+  }
+};
+
+export const hasProMembership = () => {
+  if (getUser()) {
+    return getUser().currentMembership === "pro";
+  } else {
+    return false;
+  }
+};
+export const isAuthenticated = () => {
+  const token = localStorage.getItem("sessionToken");
+  const hasMembership = getUser();
+  return (
+    hasMembership &&
+    hasMembership.currentMembership &&
+    hasMembership.userId &&
+    token
+  );
+};
+
+export const destroyLocalStorage = () => window.localStorage.clear();
+
+export const createUserProfile = (
+  username,
+  userId,
+  email,
+  currentMembership
+) => {
+  const profile = JSON.stringify({
+    ...USER_PROFILE,
+    username,
+    userId,
+    email,
+    currentMembership
+  });
+
+  localStorage.setItem("client", profile);
+};
+
+export const refresh = () => window.location.reload();
