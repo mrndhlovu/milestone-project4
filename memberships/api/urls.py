e923ccb4148d6a40ae8db6d397b2ee1ae4104815
mndhlovu <mndhlovu@hubspot.com>
2019-09-14 08:01:48 +0100

    updated memberships urls.py to handle adding to cart

--- memberships/api/urls.py
+++ memberships/api/urls.py
@@ -1,17 +1,23 @@
 from django.urls import path, re_path
-from .views import MembershipListView, MembershipPaymentView, CancelSubscriptionAPI, MembershipTransactionUpdateView, MembershipsProfileAPI, MembershipTypesAPI
+from .views import MembershipAddToCart, MembershipCheckoutView, CancelSubscriptionAPI, MembershipTransactionUpdateView, MembershipsProfileAPI, MembershipTypesAPI, MembershipDeleteFromCart, MembershipPendingOrder, MembershipCheckoutOrder
 
 app_name = 'memberships'
 
 urlpatterns = [
-    path('', MembershipListView.as_view(), name='membership_list'),
+    path('', MembershipAddToCart.as_view(), name='membership_list'),
     path('types/', MembershipTypesAPI.as_view(), name='membership_type'),
-    path('select/', MembershipListView.as_view(), name='selected_membership'),
-    path('payments/', MembershipPaymentView.as_view(), name='payments'),
+    path('add-to-cart/', MembershipAddToCart.as_view(),
+         name='add_membership_to_cart'),
+    path('remove/', MembershipDeleteFromCart.as_view(),
+         name='remove_membership_from_cart'),
+    path('payments/', MembershipCheckoutView.as_view(), name='payments'),
     path('user-profile/', MembershipsProfileAPI.as_view(), name='user_profile'),
     path('cancel-subscription/', CancelSubscriptionAPI.as_view(),
          name='cancel_subscription'),
     path('update-transaction/', MembershipTransactionUpdateView.as_view(),
-         name='update_transaction/')
-
+         name='update_transaction'),
+    path('pending-order/', MembershipPendingOrder.as_view(),
+         name='pending_order'),
+    path('checkout/', MembershipCheckoutOrder.as_view(),
+         name='checkout')
 ]
