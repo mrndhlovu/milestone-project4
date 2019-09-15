b7b797d89fa3f4d73de9f3b4f5c6c0ff6f0eb309
mndhlovu <mndhlovu@hubspot.com>
2019-09-15 17:05:59 +0100

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Home from "./components/home/Home";
-import Pricing from "./components/ecommerce/Pricing";
+import MembershipContainer from "./containers/MembershipsContainer";
 import TicketsList from "./components/tickets/TicketsList";
 import TicketDetail from "./components/tickets/TicketDetail";
 import CreateTicket from "./components/tickets/CreateTicket";
@@ -11,7 +11,7 @@ import AuthRouteCheck from "./utils/AuthRouteCheck";
 import Login from "./components/userAuth/Login";
 import Signup from "./components/userAuth/Signup";
 import Products from "./components/ecommerce/Products";
-import Cart from "./components/ecommerce/Cart";
+import CartContainer from "./containers/CartContainer";
 import CheckoutForm from "./components/ecommerce/CheckoutForm";
 import ArticleList from "./components/blog/ArticleList";
 import ArticleDetail from "./components/blog/ArticleDetail";
@@ -26,14 +26,14 @@ const BaseRouter = () => {
         <Route path="/signup" component={Signup} />
         <Route path="/tickets" component={TicketsList} />
         <Route path="/ticket/:id" component={TicketDetail} />
-        <Route path="/pricing" component={Pricing} />
+        <Route path="/pricing" component={MembershipContainer} />
         <Route path="/products" component={Products} />
         <Route path="/user-profile" component={UserProfile} />
+        <Route path="/cart" component={CartContainer} />
+        <Route path="/checkout" component={CheckoutForm} />
 
         <AuthRouteCheck path="/blog-article-list" component={ArticleList} />
         <AuthRouteCheck path="/blog-article/:id" component={ArticleDetail} />
-        <AuthRouteCheck path="/cart" component={Cart} />
-        <Route path="/checkout" component={CheckoutForm} />
         <AuthRouteCheck path="/create-ticket" component={CreateTicket} />
         <AuthRouteCheck path="/edit-ticket/:id" component={EditTicket} />
       </Switch>
