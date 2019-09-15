7c23731c5c045697becb58b48960183809b0f419
mndhlovu <mndhlovu@hubspot.com>
2019-09-15 17:04:28 +0100

    Addedngetnetprice, getcartitems, functions

--- src/utils/appCheckoutUtils.js
+++ src/utils/appCheckoutUtils.js
@@ -1,5 +1,11 @@
 import { INITIAL_STATE } from "../constants/constants";
 import { checkObjectUpdate } from "./checkObjectUpdate";
+import {
+  UNICORN_FREE_SERVICES,
+  UNICORN_PRO_SERVICES
+} from "../constants/constants";
+
+const cart = JSON.parse(localStorage.getItem("cart"));
 
 export const hasError = (state, action) => {
   return checkObjectUpdate(state, {
@@ -18,6 +24,22 @@ export const fetchingData = (state, action) => {
 export const dataReceived = (state, action) => {
   return {
     ...INITIAL_STATE,
-    data: action.payload
+    data: action.payload,
+    dataReceived: true
   };
+};
+
+export const getCartItems = () =>
+  cart && cart.membership ? cart.membership : null;
+
+export const getMembershipId = () => (cart ? cart.order.membershipId : 1);
+
+export const getOrderDesciption = slug => {
+  if (slug === "pro") return UNICORN_PRO_SERVICES;
+  else return UNICORN_FREE_SERVICES;
+};
+
+export const getNetPrice = total => {
+  const netPrice = (total * 9) / 100;
+  return total - netPrice;
 };
