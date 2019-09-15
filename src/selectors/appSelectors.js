e3419e41bb66f424f732544f83687ba6778ca5d7
mndhlovu <mndhlovu@hubspot.com>
2019-09-15 17:04:33 +0100

    Update appSelectors.js

--- src/selectors/appSelectors.js
+++ src/selectors/appSelectors.js
@@ -15,4 +15,5 @@ export const getVotes = state => state.vote;
 export const getMemberships = state => state.memberships.membershipList;
 export const getMembershipProfile = state => state.userProfile.profile;
 
-export const getCheckout = state => state.getCheckout;
+export const getCheckout = state => state.checkOut;
+export const getCheckoutDetail = state => state.checkoutDetail;
