1ade8f44db80e6eeb1c8660e17ec215f6602671a
mndhlovu <mndhlovu@hubspot.com>
2019-09-15 17:11:12 +0100

    Update CheckoutActions.js

--- src/actions/CheckoutActions.js
+++ src/actions/CheckoutActions.js
@@ -13,6 +13,8 @@ import {
   REQUEST_CHECKOUT,
   RECEIVE_ADD_T0_CART,
   REQUEST_ADD_TO_CART,
+  REQUEST_ORDER_DETAIL,
+  RECEIVE_ORDER_DETAIL,
   ERROR_ALERT
 } from "./ActionTypes";
 
@@ -22,7 +24,8 @@ import {
   requestTransactionUpdate,
   requestCheckout,
   requestPendingOrder,
-  deleteMembershipFromCart
+  deleteMembershipFromCart,
+  requestOrderItemDetail
 } from "../apis/apiRequests";
 
 import {
@@ -72,7 +75,7 @@ export const updateTranscation = () => {
     dispatch(makeRequest(UPDATE_TRANSCATION));
     requestTransactionUpdate().then(
       response => {
-        dispatch(requestSuccess(TRANSCATION_UPDATED, response));
+        dispatch(requestSuccess(TRANSCATION_UPDATED, response.data));
         dispatch(createMessage({ successMsg: response.data.message }));
         localStorage.removeItem("subscriptionId");
         localStorage.removeItem("stripeToken");
@@ -90,7 +93,7 @@ export const getPendingOrder = () => {
     dispatch(makeRequest(REQUEST_PENDING_ORDER));
     requestPendingOrder().then(
       response => {
-        dispatch(requestSuccess(RECEIVE_PENDING_ORDER, response));
+        dispatch(requestSuccess(RECEIVE_PENDING_ORDER, response.data));
         dispatch(createMessage({ successMsg: response.data.message }));
       },
       error => {
@@ -106,7 +109,7 @@ export const getCheckout = () => {
     dispatch(makeRequest(REQUEST_CHECKOUT));
     requestCheckout().then(
       response => {
-        dispatch(requestSuccess(RECEIVE_CHECKOUT, response));
+        dispatch(requestSuccess(RECEIVE_CHECKOUT, response.data));
         dispatch(createMessage({ successMsg: response.data.message }));
       },
       error => {
@@ -122,7 +125,24 @@ export const deleteFromCart = () => {
     dispatch(makeRequest(DELETE_FROM_CART));
     deleteMembershipFromCart().then(
       response => {
-        dispatch(requestSuccess(DELETED_FROM_CART, response));
+        dispatch(requestSuccess(DELETED_FROM_CART, response.data));
+        dispatch(createMessage({ successMsg: response.data.message }));
+        localStorage.removeItem("cart");
+      },
+      error => {
+        dispatch(createMessage({ successMsg: error.message }));
+        dispatch(dataRequestFail(ERROR_ALERT, error));
+      }
+    );
+  };
+};
+
+export const getOrderDetail = id => {
+  return dispatch => {
+    dispatch(makeRequest(REQUEST_ORDER_DETAIL));
+    requestOrderItemDetail(id).then(
+      response => {
+        dispatch(requestSuccess(RECEIVE_ORDER_DETAIL, response.data));
         dispatch(createMessage({ successMsg: response.data.message }));
       },
       error => {
