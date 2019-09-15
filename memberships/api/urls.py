843e2fd6cdb94044dc63623d036ab1c4832f08da
mndhlovu <mndhlovu@hubspot.com>
2019-09-15 17:11:55 +0100

    Update urls.py

--- memberships/api/urls.py
+++ memberships/api/urls.py
@@ -1,23 +1,26 @@
 from django.urls import path, re_path
-from .views import MembershipAddToCart, MembershipCheckoutView, CancelSubscriptionAPI, MembershipTransactionUpdateView, MembershipsProfileAPI, MembershipTypesAPI, MembershipDeleteFromCart, MembershipPendingOrder, MembershipCheckoutOrder
+from .views import MembershipAddToCart, CancelSubscriptionAPI, MembershipTransactionUpdateView, SubscribedUserRetrieveAPI, MembershipTypesAPI, MembershipDeleteFromCart, MembershipPendingOrderApi, MembershipCartCheckoutApi, MembershipTypesRetriveAPI
 
 app_name = 'memberships'
 
 urlpatterns = [
     path('', MembershipAddToCart.as_view(), name='membership_list'),
     path('types/', MembershipTypesAPI.as_view(), name='membership_type'),
+    path('types/<pk>/', MembershipTypesRetriveAPI.as_view(),
+         name='single_membership'),
     path('add-to-cart/', MembershipAddToCart.as_view(),
          name='add_membership_to_cart'),
     path('remove/', MembershipDeleteFromCart.as_view(),
          name='remove_membership_from_cart'),
-    path('payments/', MembershipCheckoutView.as_view(), name='payments'),
-    path('user-profile/', MembershipsProfileAPI.as_view(), name='user_profile'),
+    path('payments/', MembershipCartCheckoutApi.as_view(), name='payments'),
+    path('user-profile/<pk>/',
+         SubscribedUserRetrieveAPI.as_view(), name='subscribed_user_profile'),
     path('cancel-subscription/', CancelSubscriptionAPI.as_view(),
          name='cancel_subscription'),
     path('update-transaction/', MembershipTransactionUpdateView.as_view(),
          name='update_transaction'),
-    path('pending-order/', MembershipPendingOrder.as_view(),
+    path('pending-order/', MembershipPendingOrderApi.as_view(),
          name='pending_order'),
-    path('checkout/', MembershipCheckoutOrder.as_view(),
+    path('checkout/', MembershipCartCheckoutApi.as_view(),
          name='checkout')
 ]
