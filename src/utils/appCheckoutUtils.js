51b68c3cec80ed298e3c2e17badbd4d2f6e7ef08
mndhlovu <mndhlovu@hubspot.com>
2019-09-21 09:29:07 +0100

    Moved reducer utils to appreducerutils file

--- src/utils/appCheckoutUtils.js
+++ src/utils/appCheckoutUtils.js
@@ -1,5 +1,4 @@
-import { INITIAL_STATE } from "../constants/constants";
-import { checkObjectUpdate } from "./checkObjectUpdate";
+import { getSelectedMemberShip } from "./appUtils";
 import {
   UNICORN_FREE_SERVICES,
   UNICORN_PRO_SERVICES
@@ -7,39 +6,25 @@ import {
 
 const cart = JSON.parse(localStorage.getItem("cart"));
 
-export const hasError = (state, action) => {
-  return checkObjectUpdate(state, {
-    ...INITIAL_STATE,
-    hasError: true
-  });
-};
-
-export const fetchingData = (state, action) => {
-  return checkObjectUpdate(state, {
-    ...INITIAL_STATE,
-    isLoading: true
-  });
-};
-
-export const dataReceived = (state, action) => {
-  return {
-    ...INITIAL_STATE,
-    data: action.payload,
-    dataReceived: true
-  };
-};
-
 export const getCartItems = () =>
   cart && cart.membership ? cart.membership : null;
 
-export const getMembershipId = () => (cart ? cart.order.membershipId : 1);
+export const getMembershipId = () => {
+  const id = cart ? cart.order.membershipId : 1;
+  return id;
+};
 
-export const getOrderDesciption = slug => {
-  if (slug === "pro") return UNICORN_PRO_SERVICES;
+export const getOrderDesciption = () => {
+  if (getSelectedMemberShip() === "pro") return UNICORN_PRO_SERVICES;
   else return UNICORN_FREE_SERVICES;
 };
 
 export const getNetPrice = total => {
   const netPrice = (total * 9) / 100;
   return total - netPrice;
+};
+
+export const getTotal = price => {
+  const total = price - getNetPrice(price);
+  return total.toFixed(2);
 };
