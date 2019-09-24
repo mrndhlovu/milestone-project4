419f25290dd04d547bbfb89e0b03e0acd0739638
mndhlovu <mndhlovu@hubspot.com>
2019-09-25 00:11:04 +0100

    Update EditButtons.js

--- src/components/tickets/EditButtons.js
+++ src/components/tickets/EditButtons.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import { Button } from "semantic-ui-react";
 import { isTicketOwner } from "../../utils/appUtils";
 
-function EditButtons({ handleTicketDelete, ticketId, owner, history }) {
+function EditButtons({ handleTicketDelete, ticketId, owner }) {
   return (
     isTicketOwner(owner) && (
       <div style={{ paddingTop: 10 }}>
         <Button.Group floated="right" size="tiny">
-          <Button size="tiny" as={Link} to="/create-ticket">
+          <Button size="tiny" as={NavLink} to="/create-ticket">
             Create
           </Button>
-          <Button onClick={() => history.push(`/edit-ticket/${ticketId}`)}>
+          <Button as={NavLink} to={`/edit-ticket/${ticketId}`}>
             Edit
           </Button>
           <Button color="blue" onClick={() => handleTicketDelete()}>
