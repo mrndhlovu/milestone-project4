87869c102a91cd23b9cfa1b480d4f901f3a7cf02
mndhlovu <mndhlovu@hubspot.com>
2019-10-07 00:07:59 +0100

    Create CheckoutContainer.js

---
+++ src/containers/CheckoutContainer.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+
+import { injectStripe, Elements, StripeProvider } from "react-stripe-elements";
+import { Grid, Container, Segment, Message } from "semantic-ui-react";
+import OrderSummaryContainer from "./OrderSummaryContainer";
+
+import PaymentContainer from "./PaymentContainer";
+
+class CheckoutContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: false,
+      message: "",
+      isLoading: false
+    };
+    this.clickedSubmit = this.clickedSubmit.bind(this);
+  }
+
+  clickedSubmit() {
+    const { stripe } = this.props;
+    stripe &&
+      stripe.createToken().then(result => {
+        if (result.error) {
+          this.setState({
+            error: true,
+            isLoading: false,
+            message: result.error.message
+          });
+        } else {
+          localStorage.setItem("stripeToken", result.token.id);
+        }
+      });
+  }
+
+  render() {
+    const { error, message } = this.state;
+    return (
+      <Segment style={{ marginTop: 40 }}>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={9}>
+              <OrderSummaryContainer />
+            </Grid.Column>
+            <Grid.Column width={7}>
+              {error && (
+                <Message negative>
+                  <Message.Header>Payment not successful!</Message.Header>
+                  <p>{message}</p>
+                </Message>
+              )}
+
+              <PaymentContainer clickedSubmit={this.clickedSubmit} />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    );
+  }
+}
+
+const FormInjection = injectStripe(CheckoutContainer);
+
+const StripeFormWrapper = () => (
+  <StripeProvider apiKey="pk_test_wbwSPsjAPpig1d7f4nP97LsX00zgMUqGbp">
+    <Container>
+      <Elements>
+        <FormInjection />
+      </Elements>
+    </Container>
+  </StripeProvider>
+);
+
+export default StripeFormWrapper;
