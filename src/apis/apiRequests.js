5895923b55a0f2ad629e6383d076fabea262108a
mndhlovu <mndhlovu@hubspot.com>
2019-10-07 00:08:38 +0100

    Update apiRequests.js

--- src/apis/apiRequests.js
+++ src/apis/apiRequests.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 import {
-  getRootUrl,
+  AUTH_EP,
   authQueryParams,
+  COMMENTS_EP,
+  MEMBERSHIP_EP,
   params,
-  queryParams
+  queryParams,
+  TICKETS_EP,
+  CHECKOUT_EP,
+  getCheckoutBody
 } from "../utils/urls";
 
 import { SESSION_TOKEN } from "../constants/localStorageConstants";
@@ -14,169 +19,120 @@ import {
   getChoosenMembership
 } from "../utils/appUtils";
 
-import { getCartItems } from "../utils/appCheckoutUtils";
-
-const cartItem = getCartItems();
-
 export async function requestTicketsList() {
-  return axios.get(`${getRootUrl}/tickets/`);
+  return axios.get(`${TICKETS_EP}`);
 }
 
 export async function requestTicketUpdate(id, body) {
-  return axios.put(`${getRootUrl}/tickets/update/${id}/`, body);
+  return axios.put(`${TICKETS_EP}update/${id}/`, body);
 }
 
 export async function requestTicketDelete(id) {
-  return axios.delete(`${getRootUrl}/tickets/delete/${id}/`, authQueryParams);
+  return axios.delete(`${TICKETS_EP}delete/${id}/`, authQueryParams);
 }
 
 export async function requestTicketVoteUpdate(id) {
-  return axios.get(`${getRootUrl}/tickets/api/${id}/vote/`, authQueryParams);
-}
-
-export async function requestProductsList() {
-  return axios.get(`${getRootUrl}/products/`);
+  return axios.get(`${TICKETS_EP}api/${id}/vote/`, authQueryParams);
 }
 
 export async function requestMembershipsList() {
-  return axios.get(`${getRootUrl}/memberships/types/`);
+  return axios.get(`${MEMBERSHIP_EP}`);
 }
 
 export async function requestUserMembershipsProfile() {
-  return axios.post(
-    `${getRootUrl}/memberships/member-profile/`,
-    null,
-    authQueryParams
-  );
+  return axios.post(`${MEMBERSHIP_EP}member-profile/`, null, authQueryParams);
 }
 
 export async function requestCancelSubsricption() {
   return axios.post(
-    `${getRootUrl}/memberships/cancel-subscription/`,
+    `${MEMBERSHIP_EP}cancel-subscription/`,
     null,
     authQueryParams
   );
 }
 
-export async function addMembershipToCart() {
+export async function requestUser() {
+  return axios.get(`${AUTH_EP}user`, authQueryParams);
+}
+
+export async function requestUserProfile() {
+  return axios.get(`${AUTH_EP}profile/`, authQueryParams);
+}
+
+export async function requestUserProfileDetail(id) {
+  return axios.get(`${AUTH_EP}profile/${id}/`, authQueryParams);
+}
+
+export async function requestSignup(body) {
+  return axios.post(`${AUTH_EP}signup`, body, queryParams);
+}
+
+export async function requestLogin(body) {
+  return axios.post(`${AUTH_EP}login`, body, queryParams);
+}
+
+export async function requestLogout() {
+  return axios.post(`${AUTH_EP}logout`, null, authQueryParams);
+}
+
+export async function requestCreateTicket(body) {
+  return axios.post(`${TICKETS_EP}api/create/`, body, authQueryParams);
+}
+
+export async function fetchTicketDetail(id) {
+  return axios.get(`${TICKETS_EP}${id}/`);
+}
+
+export async function requestTicketComments() {
+  return axios.get(`${COMMENTS_EP}`);
+}
+
+export async function requestCreateComment(body) {
+  return axios.post(`${COMMENTS_EP}create-comment/`, body, authQueryParams);
+}
+
+export async function requestCreateReply(body) {
+  return axios.post(`${COMMENTS_EP}create-reply/`, body, authQueryParams);
+}
+
+// Checkout
+export async function requestAddItemToCart(id, productType) {
   return axios.post(
-    `${getRootUrl}/memberships/add-to-cart/`,
-    getMembershipType(),
+    `${CHECKOUT_EP}/add-to-cart/`,
+    getCheckoutBody(id, productType),
     authQueryParams
   );
 }
 
-export async function deleteMembershipFromCart() {
+export async function requestPendingOrder() {
+  return axios.get(`${CHECKOUT_EP}/pending-order/`, authQueryParams);
+}
+
+export const requestRemoveItemFromCart = (id, productType) => {
   return axios.post(
-    `${getRootUrl}/memberships/remove/`,
-    getMembershipType(cartItem),
+    `${CHECKOUT_EP}/remove-item/`,
+    getCheckoutBody(id, productType),
     authQueryParams
   );
-}
+};
 
-export async function requestOrderItemDetail(id) {
-  return axios.get(`${getRootUrl}/memberships/types/${id}/`, authQueryParams);
-}
+export const requestItemPayment = () => {
+  const subscriptionId = localStorage.getItem("subscriptionId");
+  let body = { stripeToken: localStorage.getItem("stripeToken") };
 
-export async function requestMembershipPayment() {
-  params.headers["Authorization"] = `Token ${SESSION_TOKEN}`;
+  if (subscriptionId !== "null") {
+    body = { ...body, subscriptionId };
+  }
 
-  return axios.post(
-    `${getRootUrl}/memberships/payments/`,
-    getChoosenMembership(),
-    params
-  );
-}
+  return axios.post(`${CHECKOUT_EP}/checkout/`, body, authQueryParams);
+};
 
 export async function requestTransactionUpdate() {
   params.headers["Authorization"] = `Token ${SESSION_TOKEN}`;
 
   return axios.post(
-    `${getRootUrl}/memberships/update-transaction/`,
+    `${CHECKOUT_EP}/update-transaction/`,
     getTransactionUpdate(),
     params
-  );
-}
-
-export async function requestCheckout() {
-  params.headers["Authorization"] = `Token ${SESSION_TOKEN}`;
-
-  return axios.post(
-    `${getRootUrl}/memberships/checkout/`,
-    getMembershipType(cartItem),
-    params
-  );
-}
-
-export async function requestPendingOrder() {
-  params.headers["Authorization"] = `Token ${SESSION_TOKEN}`;
-
-  return axios.post(
-    `${getRootUrl}/memberships/pending-order/`,
-    getMembershipType(cartItem),
-    params
-  );
-}
-
-export async function requestUser() {
-  return axios.get(`${getRootUrl}/accounts/api/auth/user`, authQueryParams);
-}
-
-export async function requestUserProfile() {
-  return axios.get(`${getRootUrl}/accounts/api/auth/profile/`, authQueryParams);
-}
-
-export async function requestUserProfileDetail(id) {
-  return axios.get(
-    `${getRootUrl}/accounts/api/auth/profile/${id}/`,
-    authQueryParams
-  );
-}
-
-export async function requestSignup(body) {
-  return axios.post(
-    `${getRootUrl}/accounts/api/auth/signup`,
-    body,
-    queryParams
-  );
-}
-
-export async function requestLogin(body) {
-  return axios.post(`${getRootUrl}/accounts/api/auth/login`, body, queryParams);
-}
-
-export async function requestLogout() {
-  return axios.post(
-    `${getRootUrl}/accounts/api/auth/logout`,
-    null,
-    authQueryParams
-  );
-}
-
-export async function requestCreateTicket(body) {
-  return axios.post(`${getRootUrl}/tickets/api/create/`, body, authQueryParams);
-}
-
-export async function fetchTicketDetail(id) {
-  return axios.get(`${getRootUrl}/tickets/${id}/`);
-}
-
-export async function requestTicketComments() {
-  return axios.get(`${getRootUrl}/comments/`);
-}
-
-export async function requestCreateComment(body) {
-  return axios.post(
-    `${getRootUrl}/comments/create-comment/`,
-    body,
-    authQueryParams
-  );
-}
-
-export async function requestCreateReply(body) {
-  return axios.post(
-    `${getRootUrl}/comments/create-reply/`,
-    body,
-    authQueryParams
   );
 }
