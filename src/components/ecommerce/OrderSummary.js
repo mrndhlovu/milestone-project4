1ba3e4c33352b949afb453fa9de441f7b8cef4ed
mndhlovu <mndhlovu@hubspot.com>
2019-10-07 00:09:01 +0100

    Update OrderSummary.js

--- src/components/ecommerce/OrderSummary.js
+++ src/components/ecommerce/OrderSummary.js
@@ -1,83 +1,62 @@
-import React, { Component } from "react";
+import React from "react";
 
-import { Card, Input, List, Button } from "semantic-ui-react";
+import { Card, Table, List, Header } from "semantic-ui-react";
+import UILoadingSpinner from "../sharedComponents/UILoadingSpinner";
 
-export class OrderSummary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      itemPrice: "",
-      itemName: "",
-      itemDesription: "",
-      showCouponInput: "",
-      defaultCouponButtonText: true
-    };
-    this.handleHasCouponClick = this.handleHasCouponClick.bind(this);
-    this.renderCouponInput = this.renderCouponInput.bind(this);
-  }
+const OrderSummary = ({ pendingOrders, handleRemoveClick, history }) => {
+  const orderItems = pendingOrders.orders ? pendingOrders.orders : {};
 
-  handleHasCouponClick(buttonText) {
-    buttonText === "Have a coupon? Click here to enter your code"
-      ? this.setState({
-          showCouponInput: true,
-          defaultCouponButtonText: !this.state.defaultCouponButtonText
-        })
-      : this.setState({
-          showCouponInput: false,
-          defaultCouponButtonText: true
-        });
-  }
+  const renderItems = () => {
+    return Object.keys(orderItems).map((item, index) => {
+      const { ticket, membership } = orderItems[item];
+      const name = orderItems[item].ticket ? ticket : `Unicorn ${membership}`;
+      const { id, price } = orderItems[item];
+      const productId = ticket ? "ticket" : "membership";
 
-  renderCouponInput() {
-    const { showCouponInput } = this.state;
-    return showCouponInput ? (
-      <Card.Content>
-        <Input
-          action={{
-            color: "grey",
-            labelPosition: "left",
-            icon: "cart",
-            content: "Apply Discount"
-          }}
-          actionPosition="left"
-          placeholder="e.g: DISCOUNT10"
-        />
-      </Card.Content>
-    ) : null;
-  }
-  renderCartItem() {
-    const { itemPrice, itemDesription, itemName } = this.state;
-    return (
-      <Card.Content>
-        <Card.Header content={`Order Item Name: ${itemName}`} />
-        <Card.Meta content={`Order Item:  ${itemDesription}`} />
-        <Card.Description content={`Price: ${itemPrice}`} />
-      </Card.Content>
-    );
-  }
+      return (
+        <Table.Row key={index}>
+          <Table.Cell>
+            <List.Header as="a" onClick={() => history.push(`ticket/${id}`)}>
+              <span style={{ fontWeight: 700, color: "black" }}>
+                {name.toUpperCase()}
+              </span>
+            </List.Header>
+          </Table.Cell>
+          <Table.Cell>${price}</Table.Cell>
+          <Table.Cell>
+            <span style={{ paddingRight: 30 }}>${price}</span>
+            <List.Header
+              as="a"
+              onClick={() => handleRemoveClick(id, productId)}
+            >
+              <span style={{ fontSize: 10, color: "grey" }}>Remove</span>
+            </List.Header>
+          </Table.Cell>
+        </Table.Row>
+      );
+    });
+  };
 
-  render() {
-    const { showCouponInput, defaultCouponButtonText } = this.state;
-    const buttonText = defaultCouponButtonText
-      ? "Have a coupon? Click here to enter your code"
-      : "Dont have coupon";
-    return (
-      <div>
-        <Card fluid>
-          <Card.Content header="Order Summary" />
-          {this.renderCartItem()}
+  return pendingOrders !== {} ? (
+    <Card.Content>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Item</Table.HeaderCell>
+            <Table.HeaderCell>Item Price</Table.HeaderCell>
+            <Table.HeaderCell>Total Price</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>{renderItems()}</Table.Body>
+      </Table>
 
-          <List.Content>
-            <Button basic onClick={() => this.handleHasCouponClick(buttonText)}>
-              {buttonText}
-            </Button>
-          </List.Content>
-
-          {showCouponInput ? this.renderCouponInput() : null}
-        </Card>
-      </div>
-    );
-  }
-}
+      <Header as="h5" floated="right">
+        <span>Total: $ {pendingOrders.total}</span>
+      </Header>
+    </Card.Content>
+  ) : (
+    <UILoadingSpinner />
+  );
+};
 
 export default OrderSummary;
