5f47913f9f8ccef59f79362d2cfa1a05b6411880
mndhlovu <mndhlovu@hubspot.com>
2019-10-07 00:08:08 +0100

    Update TicketListWrapper.js

--- src/components/tickets/TicketListWrapper.js
+++ src/components/tickets/TicketListWrapper.js
@@ -1,21 +1,22 @@
 import React from "react";
 
-import { Container, Header, List, Segment } from "semantic-ui-react";
+import { Container, Header, Segment, Card } from "semantic-ui-react";
 
 const TicketListWrapper = ({ children, ticketCount, isLoading }) => {
   return (
     <Container>
-      <Header
-        content="TICKETS"
-        subheader={`TOTAL:  ${ticketCount}`}
-        as="h3"
-        style={{ paddingTop: 20 }}
-      />
-      <Segment padded loading={isLoading} size="tiny">
-        <List divided relaxed>
+      <Card fluid>
+        <Header
+          content="TICKETS"
+          subheader={`TOTAL:  ${ticketCount}`}
+          as="h3"
+          style={{ paddingTop: 20 }}
+          attached
+        />
+        <Segment attached padded loading={isLoading} size="tiny">
           {children}
-        </List>
-      </Segment>
+        </Segment>
+      </Card>
     </Container>
   );
 };
