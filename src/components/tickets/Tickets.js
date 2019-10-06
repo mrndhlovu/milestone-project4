2b90d0432616563dae602db8b4074ab12c45aac8
mndhlovu <mndhlovu@hubspot.com>
2019-10-07 00:10:02 +0100

    Update Tickets.js

--- src/components/tickets/Tickets.js
+++ src/components/tickets/Tickets.js
@@ -1,11 +1,11 @@
 import React, { Fragment } from "react";
 
-import { Label, Feed, Divider } from "semantic-ui-react";
+import { Label, Feed, Divider, Button } from "semantic-ui-react";
 
 import TicketsHeader from "./TicketsHeader";
 import TicketsIcons from "./TicketsIcon";
 
-export const Tickets = ({ ticketsList }) => {
+export const Tickets = ({ ticketsList, handleAddToCart }) => {
   return ticketsList.map(ticket => {
     const {
       title,
@@ -16,7 +16,8 @@ export const Tickets = ({ ticketsList }) => {
       votes,
       username,
       status,
-      short_desc
+      short_desc,
+      price
     } = ticket;
 
     return (
@@ -36,10 +37,19 @@ export const Tickets = ({ ticketsList }) => {
             <TicketsIcons votes={votes} status={status} views={views} />
           </Feed.Event>
         </Feed>
-
+        <Button
+          floated="right"
+          size="tiny"
+          color="orange"
+          onClick={() => handleAddToCart(id)}
+        >
+          Add to Cart
+        </Button>
         <Label.Group size="tiny">
           <Label>{tag}</Label>
+          <Label>${price}</Label>
         </Label.Group>
+
         <Divider />
       </Fragment>
     );
