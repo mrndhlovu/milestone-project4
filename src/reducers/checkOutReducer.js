1b48bac9d32bfb4cc8c1f01a0fdfb0dd99c26d09
mndhlovu <mndhlovu@hubspot.com>
2019-10-09 17:26:18 +0100

    Update checkOutReducer.js

--- src/reducers/checkOutReducer.js
+++ src/reducers/checkOutReducer.js
@@ -15,11 +15,25 @@ import { hasError, fetchingData, dataReceived } from "../utils/appReducersUtil";
 
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case UPDATE_TRANSCATION || SUBMITTING_PAYMENT || REQUEST_CHECKOUT:
+    case UPDATE_TRANSCATION:
       return fetchingData(state, action);
-    case TRANSCATION_UPDATE_FAIL || PAYMENT_FAIL || CHECKOUT_ERROR:
+    case SUBMITTING_PAYMENT:
+      return fetchingData(state, action);
+    case REQUEST_CHECKOUT:
+      return fetchingData(state, action);
+
+    case TRANSCATION_UPDATE_FAIL:
       return hasError(state, action);
-    case TRANSCATION_UPDATED || PAYMENT_SUCCESS || RECEIVE_CHECKOUT:
+    case PAYMENT_FAIL:
+      return hasError(state, action);
+    case CHECKOUT_ERROR:
+      return hasError(state, action);
+
+    case TRANSCATION_UPDATED:
+      return dataReceived(state, action);
+    case PAYMENT_SUCCESS:
+      return dataReceived(state, action);
+    case RECEIVE_CHECKOUT:
       return dataReceived(state, action);
     default:
       return state;
