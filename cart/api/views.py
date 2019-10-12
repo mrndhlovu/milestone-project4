3cef94e663b4988ec05f610753cef8972eb20bd9
mndhlovu <mndhlovu@hubspot.com>
2019-10-12 22:19:49 +0100

    code clean up

--- cart/api/views.py
+++ cart/api/views.py
@@ -22,6 +22,7 @@ from django.utils import timezone
 from cart.models import Cart, CartItem, CartPayment
 from django.contrib.contenttypes.models import ContentType
 import stripe
+import traceback
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -76,6 +77,7 @@ def updated_user_membership(request, data):
     user_membership = get_user_membership(request)
 
     user_membership.is_pro_member = True
+    user_membership.membership = get_object_or_404(Membership, id=2)
     user_membership.save()
     try:
         subscription_id = data['subscriptionId']
@@ -91,8 +93,8 @@ def updated_user_membership(request, data):
         }
 
         return context
-    except:
-        return None
+    except Exception:
+        print(traceback.print_exc())
 
 
 def get_ticket(item):
@@ -117,14 +119,17 @@ def get_subscription_id(request, data):
         choosen_membership = get_object_or_404(
             Membership, id=data['product_id'])
 
-    except:
-        return None
+    except Exception:
+        print(traceback.print_exc())
     try:
         token = data['stripeToken']
 
         customer = stripe.Customer.retrieve(
             user_membership.stripe_customer_id)
         customer.source = token
+        customer.save()
+
+        print(customer)
 
         subscription = stripe.Subscription.create(
             customer=user_membership.stripe_customer_id,
@@ -139,8 +144,24 @@ def get_subscription_id(request, data):
 
         return subscription_id
 
-    except:
-        return None
+    except Exception:
+        print(traceback.print_exc())
+
+
+def get_stripe_customer(request, token):
+    user = UserProfile.objects.filter(user=request.user)
+
+    if user.exists():
+        user_profile = user.first()
+        existing_customer = stripe.Customer.retrieve(
+            user_profile.stripe_customer_id)
+
+        existing_customer.sources.create(source=token)
+        existing_customer.save()
+        return existing_customer
+
+    else:
+        return False
 
 
 class AddToCartAPIView(ListAPIView):
@@ -153,6 +174,8 @@ class AddToCartAPIView(ListAPIView):
 
         product_id = request_data['product_id']
         product_object = request_data['product']
+        token = request_data['stripeToken']
+
         cart = get_active_cart(request)
         subscription_id = None
 
@@ -166,20 +189,26 @@ class AddToCartAPIView(ListAPIView):
             if product.id == 2:
                 product.membership_type = 'pro'
 
-            update_selected_membership(request, product)
-
         if cart is not None and cart.is_paid is False:
-            product_content_type = ContentType.objects.get_for_model(product)
+            product_content_type = ContentType.objects.get_for_model(
+                product)
             pending_orders_qs = CartItem.objects.filter(
                 product_content_type=product_content_type)
 
-            cart.add_item(product)
+            for item in pending_orders_qs:
+                if item.product_object_id == product.id:
+                    context = {
+                        'message': 'Item already in your cart.',
+                    }
+                    return JsonResponse(context, status=status.HTTP_200_OK)
 
+            cart.add_item(product)
             context = {
                 'message': 'Item added to cart',
                 'subscription_id': subscription_id
             }
             return JsonResponse(context, status=status.HTTP_200_OK)
+
         try:
             cart, created = Cart.objects.get_or_create(user=request.user)
             cart.add_item(product)
@@ -190,11 +219,8 @@ class AddToCartAPIView(ListAPIView):
             }
             return JsonResponse(context, status=status.HTTP_200_OK)
 
-        except:
-            context = {
-                'message': 'Error adding item to cart'
-            }
-            return JsonResponse(context, status=status.HTTP_400_BAD_REQUEST, )
+        except Exception:
+            print(traceback.print_exc())
 
 
 class OrderDetailAPIView(APIView):
@@ -230,11 +256,8 @@ class OrderDetailAPIView(APIView):
                 }
                 return Response(context, status=status.HTTP_200_OK)
 
-        except:
-            context = {
-                'message': "No cart found!",
-            }
-            return Response(context, status=status.HTTP_400_BAD_REQUEST)
+        except Exception:
+            print(traceback.print_exc())
 
 
 class CartRemoveItemAPIView(RetrieveAPIView):
@@ -276,11 +299,8 @@ class CartRemoveItemAPIView(RetrieveAPIView):
                 }
                 return Response(context, status=status.HTTP_404_NOT_FOUND)
 
-        except:
-            context = {
-                'message': "Error removing item from cart"
-            }
-            return Response(context, status=status.HTTP_404_NOT_FOUND)
+        except Exception:
+            print(traceback.print_exc())
 
 
 class PaymentAPIView(APIView):
@@ -295,109 +315,114 @@ class PaymentAPIView(APIView):
         items = pending_order.items.all()
         total = sum(item.product.price for item in items) * 100
 
-        userprofile = UserProfile.objects.get(user=request.user)
+        user_profile = UserProfile.objects.get(user=request.user)
 
-        customer = stripe.Customer.retrieve(
-            userprofile.stripe_customer_id)
-        customer.sources.create(source=token)
-        customer.save()
+        customer = get_stripe_customer(request, token)
+        if customer is not None:
 
-        try:
-            if customer:
+            try:
+                if customer:
 
-                charge = stripe.Charge.create(
-                    amount=total,
-                    currency="eur",
-                    customer=userprofile.stripe_customer_id
-                )
-            else:
-                charge = stripe.Charge.create(
-                    amount=total,
-                    currency="eur",
-                    source=token
-                )
+                    charge = stripe.Charge.create(
+                        amount=total,
+                        currency="eur",
+                        customer=user_profile.stripe_customer_id
+                    )
+                else:
+                    charge = stripe.Charge.create(
+                        amount=total,
+                        currency="eur",
+                        source=token
+                    )
 
-            payment = CartPayment()
-            payment.stripe_charge_id = charge['id']
-            payment.user = self.request.user
-            payment.amount = total
-            pending_order.is_paid = True
+                payment = CartPayment()
+                payment.stripe_charge_id = charge['id']
+                payment.user = self.request.user
+                payment.amount = total
+                pending_order.is_paid = True
 
-            if pending_order.is_paid:
-                pending_order.save()
-                payment.save()
+                if pending_order.is_paid:
+                    pending_order.save()
+                    payment.save()
 
-                updated_user_membership(request, request_data)
-                paid_cart = get_paid_cart(request)
-                paid_cart.delete()
+                    updated_user_membership(request, request_data)
+                    paid_cart = get_paid_cart(request)
+                    paid_cart.delete()
 
-                user_profile = UserProfile.objects.get(user=request.user)
-                if user_profile is not None:
-                    for item in items:
+                    user_profile = UserProfile.objects.get(user=request.user)
+                    if user_profile is not None:
+                        for item in items:
 
-                        if item.product_content_type.name == 'membership':
-                            membership = get_membership(item)
-                            user_profile.active_membership.add(membership)
-                            user_profile.save()
+                            if item.product_content_type.name == 'membership':
+                                free_membership = get_object_or_404(
+                                    Membership, id=1)
+                                membership = get_membership(item)
+                                user_profile.active_membership.add(membership)
+                                user_profile.active_membership.remove(
+                                    free_membership)
+                                user_profile.save()
 
-                        if item.product_content_type.name == 'ticket':
-                            ticket = get_ticket(item)
-                            user_profile.paid_tickets.add(ticket)
-                            user_profile.save()
+                            if item.product_content_type.name == 'ticket':
+                                ticket = get_ticket(item)
+                                user_profile.paid_tickets.add(ticket)
+                                user_profile.save()
 
-                            ticket_solution, created = TicketSolution.objects.get_or_create(
-                                parent_ticket_id=ticket.id)
-                            ticket_solution.paid_client.add(request.user)
-                            ticket_solution.status = 'doing'
-                            ticket_solution.save()
+                                ticket_solution, created = TicketSolution.objects.get_or_create(
+                                    parent_ticket_id=ticket.id)
+                                ticket_solution.paid_client.add(request.user)
+                                ticket_solution.status = 'doing'
+                                ticket_solution.save()
+
+                    context = {
+                        'message': "Payment was successful!"
+                    }
+                    return Response(context, status=status.HTTP_200_OK)
+                else:
+                    context = {
+                        'message': "Error saving your subscription successful!"
+                    }
+                    return Response(context, status=status.HTTP_400_BAD_REQUEST)
+
+            except stripe.error.CardError as e:
+                body = e.json_body
+                err = body.get('error', {})
 
                 context = {
-                    'message': "Payment was successful!"
-                }
-                return Response(context, status=status.HTTP_200_OK)
-            else:
-                context = {
-                    'message': "Error saving your subscription successful!"
+                    'message': f"{err.get('message')}"
                 }
                 return Response(context, status=status.HTTP_400_BAD_REQUEST)
 
-        except stripe.error.CardError as e:
-            body = e.json_body
-            err = body.get('error', {})
+            except stripe.error.RateLimitError:
+                context = {
+                    'message': "Rate limit error | Too many request"
+                }
+                return Response(context, status=status.HTTP_400_BAD_REQUEST)
 
-            context = {
-                'message': f"{err.get('message')}"
-            }
-            return Response(context, status=status.HTTP_400_BAD_REQUEST)
+            except stripe.error.InvalidRequestError:
+                context = {
+                    'message': "Invalid parameters"
+                }
+                return Response(context, status=status.HTTP_400_BAD_REQUEST)
 
-        except stripe.error.RateLimitError:
-            context = {
-                'message': "Rate limit error | Too many request"
-            }
-            return Response(context, status=status.HTTP_400_BAD_REQUEST)
+            except stripe.error.AuthenticationError:
+                context = {
+                    'message': "Not Authenticated"
+                }
+                return Response(context, status=status.HTTP_400_BAD_REQUEST)
 
-        except stripe.error.InvalidRequestError:
-            context = {
-                'message': "Invalid parameters"
-            }
-            return Response(context, status=status.HTTP_400_BAD_REQUEST)
+            except stripe.error.APIConnectionError:
+                context = {
+                    'message': "Network error"
+                }
+                return Response(context, status=status.HTTP_400_BAD_REQUEST)
 
-        except stripe.error.AuthenticationError:
-            context = {
-                'message': "Not Authenticated"
-            }
-            return Response(context, status=status.HTTP_400_BAD_REQUEST)
-
-        except stripe.error.APIConnectionError:
-            context = {
-                'message': "Network error"
-            }
-            return Response(context, status=status.HTTP_400_BAD_REQUEST)
-
-        except stripe.error.StripeError as e:
-            context = {
-                'message': "Something went wrong. You were not charged. Please try again."
-            }
+            except stripe.error.StripeError as e:
+                context = {
+                    'message': "Something went wrong. You were not charged. Please try again."
+                }
+                return Response(context, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            context = {'message': "Customer has no stripe profile"}
             return Response(context, status=status.HTTP_400_BAD_REQUEST)
 
 
