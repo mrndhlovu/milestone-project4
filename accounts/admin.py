9be9caaabde0c3cf07c1a6486c12ec105850d253
mndhlovu <mndhlovu@hubspot.com>
2019-10-12 22:10:37 +0100

    Creatted a donations app
    setup model and registed on django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,14 +5,13 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from memberships.models import Membership, UserMembership, Subscription
 from tickets.models import Ticket, TicketSolution
-from cart.models import Cart, CartItem, CartPayment
+from cart.models import Cart, CartItem, CartPayment, Donation
 
 User = get_user_model()
 
 admin.site.unregister(User)
 admin.site.register(User)
 admin.site.register(UserProfile)
-
 
 admin.site.register(Membership)
 admin.site.register(UserMembership)
@@ -25,3 +24,4 @@ admin.site.register(Comment)
 admin.site.register(Cart)
 admin.site.register(CartItem)
 admin.site.register(CartPayment)
+admin.site.register(Donation)
