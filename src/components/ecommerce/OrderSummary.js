c1533bb08c9948dbe41f36c5ce62e27b2deb238e
mndhlovu <mndhlovu@hubspot.com>
2019-10-18 00:53:18 +0100

    handle donations being added to cart

--- src/components/ecommerce/OrderSummary.js
+++ src/components/ecommerce/OrderSummary.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Table, List, Icon, Button, Label } from "semantic-ui-react";
+import { Table, List, Icon, Button, Label, Card } from "semantic-ui-react";
 import UILoadingSpinner from "../sharedComponents/UILoadingSpinner";
 
 const OrderSummary = ({
@@ -15,9 +15,19 @@ const OrderSummary = ({
   const renderItems = () => {
     return Object.keys(orderItems).map((item, index) => {
       const { ticket, membership } = orderItems[item];
-      const name = orderItems[item].ticket ? ticket : `Unicorn ${membership}`;
       const { id, price } = orderItems[item];
-      const productId = ticket ? "ticket" : "membership";
+
+      const name = orderItems[item].ticket
+        ? ticket
+        : membership
+        ? `Unicorn ${membership}`
+        : `[Kind Donation]`;
+
+      const productId = ticket
+        ? "ticket"
+        : membership
+        ? "membership"
+        : "donation";
 
       return (
         <Table.Row key={index}>
@@ -52,36 +62,38 @@ const OrderSummary = ({
   };
 
   return pendingOrders !== {} ? (
-    <Table stackable>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell colSpan="6">
-            Your have {count} items in your cart
-          </Table.HeaderCell>
-        </Table.Row>
+    <Card fluid>
+      <Table stackable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell colSpan="6">
+              Your have {count} items in your cart
+            </Table.HeaderCell>
+          </Table.Row>
 
-        <Table.Row>
-          <Table.HeaderCell>ID</Table.HeaderCell>
-          <Table.HeaderCell>Item name</Table.HeaderCell>
-          <Table.HeaderCell>Subtotal</Table.HeaderCell>
-          <Table.HeaderCell>Total</Table.HeaderCell>
-          <Table.HeaderCell textAlign="right">Confirm</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>{renderItems()}</Table.Body>
-      <Table.Footer>
-        <Table.Row>
-          <Table.HeaderCell colSpan="6" textAlign="right">
-            <Button as="div" labelPosition="right">
-              <Button color="orange">Total €</Button>
-              <Label as="a" basic pointing="left">
-                {pendingOrders.total}
-              </Label>
-            </Button>
-          </Table.HeaderCell>
-        </Table.Row>
-      </Table.Footer>
-    </Table>
+          <Table.Row>
+            <Table.HeaderCell>ID</Table.HeaderCell>
+            <Table.HeaderCell>Item name</Table.HeaderCell>
+            <Table.HeaderCell>Subtotal</Table.HeaderCell>
+            <Table.HeaderCell>Total</Table.HeaderCell>
+            <Table.HeaderCell textAlign="right">Confirm</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>{renderItems()}</Table.Body>
+        <Table.Footer>
+          <Table.Row>
+            <Table.HeaderCell colSpan="6" textAlign="right">
+              <Button as="div" labelPosition="right">
+                <Button color="orange">Total €</Button>
+                <Label as="a" basic pointing="left">
+                  {pendingOrders.total}
+                </Label>
+              </Button>
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
+    </Card>
   ) : (
     <UILoadingSpinner />
   );
