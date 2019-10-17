e5df957780729146238b91f80e9f2c1e521bac42
mndhlovu <mndhlovu@hubspot.com>
2019-10-18 00:52:51 +0100

    updated donations component to have buttons and custom amount input

--- src/components/ecommerce/Donations.js
+++ src/components/ecommerce/Donations.js
@@ -1,23 +1,62 @@
 import React from "react";
-import { Card, Input } from "semantic-ui-react";
+import {
+  Card,
+  Grid,
+  Menu,
+  Button,
+  Input,
+  Header,
+  Segment
+} from "semantic-ui-react";
 
-const Donations = ({ handleDonationInput, donateIsDisable, buttonText }) => {
+import { DONATION_AMOUNTS } from "../../constants/constants";
+
+const Donations = ({ handleAddToCart, handleDonationInput, buttonText }) => {
+  const otherAmount = () => {
+    return (
+      <Card.Content style={{ paddingBottom: 10 }}>
+        <Input
+          onChange={event => handleDonationInput(event)}
+          onBlur={() => handleAddToCart("other")}
+          fluid
+          action={{
+            color: "blue",
+            labelPosition: "left",
+            icon: "euro",
+            content: `${buttonText}`
+          }}
+          placeholder="other amount"
+        />
+      </Card.Content>
+    );
+  };
+  const renderDonationCards = () => {
+    return Object.keys(DONATION_AMOUNTS).map(key => {
+      return (
+        <Grid.Column key={key} style={{ paddingBottom: 10 }}>
+          <Menu fluid>
+            <Button
+              color="blue"
+              fluid
+              onClick={() => handleAddToCart("button", DONATION_AMOUNTS[key])}
+            >
+              € {DONATION_AMOUNTS[key]}
+            </Button>
+          </Menu>
+        </Grid.Column>
+      );
+    });
+  };
+
   return (
-    <Card.Content>
-      <Input
-        onChange={event => handleDonationInput(event)}
-        disabled={donateIsDisable}
-        fluid
-        action={{
-          color: "orange",
-          labelPosition: "left",
-          icon: "heart",
-          content: `${buttonText}`
-        }}
-        actionPosition="left"
-        placeholder="amount €"
-      />
-    </Card.Content>
+    <Segment>
+      <Header content="Donate" />
+
+      <Grid columns={3}>
+        <Grid.Row>{renderDonationCards()}</Grid.Row>
+      </Grid>
+      {otherAmount()}
+    </Segment>
   );
 };
 
