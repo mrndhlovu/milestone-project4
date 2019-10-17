59aa58e098f077148c986da03a846f67fe859980
mndhlovu <mndhlovu@hubspot.com>
2019-10-18 00:55:41 +0100

    handle donation input and request add to cart

--- src/containers/OrderSummaryContainer.js
+++ src/containers/OrderSummaryContainer.js
@@ -1,8 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-
-import { Card } from "semantic-ui-react";
 
 import OrderSummary from "../components/ecommerce/OrderSummary";
 import {
@@ -10,29 +8,67 @@ import {
   removeItemFromCart
 } from "../actions/CheckoutActions";
 import { getCartPendingOrder } from "../selectors/appSelectors";
+import Donations from "../components/ecommerce/Donations";
+import { addItemToCart } from "../actions/CheckoutActions";
 
 export class OrderSummaryContainer extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      donateIsDisable: true,
+      buttonText: "Other amount",
+      donation: 0
+    };
+
     this.handleRemoveClick = this.handleRemoveClick.bind(this);
+    this.handleAddToCart = this.handleAddToCart.bind(this);
+    this.handleDonationInput = this.handleDonationInput.bind(this);
   }
 
   handleRemoveClick(id, productId) {
     this.props.removeItemFromCart(id, productId);
   }
 
+  handleDonationInput(e) {
+    // from stackoverflow
+    const re = /^[0-9\b]+$/;
+    if (e.target.value === "" || re.test(e.target.value)) {
+      this.setState({ donation: e.target.value });
+    }
+  }
+
+  handleAddToCart(type, amount) {
+    const { donation } = this.state;
+    if (type === "button") {
+      this.props.addItemToCart(null, "donation", amount);
+    } else {
+      const otherAmount = parseInt(donation);
+
+      this.props.addItemToCart(null, "donation", otherAmount);
+    }
+  }
+
   render() {
+    const { donateIsDisable, buttonText } = this.state;
     const { pendingOrders, history } = this.props;
     const count = pendingOrders.data.count ? pendingOrders.data.count : 0;
     return (
-      <Card fluid>
+      <Fragment>
         <OrderSummary
           pendingOrders={pendingOrders.data}
           handleRemoveClick={this.handleRemoveClick}
           history={history}
           count={count}
         />
-      </Card>
+
+        <Donations
+          handleDonationInput={this.handleDonationInput}
+          handlePayNow={this.handlePayNow}
+          donateIsDisable={donateIsDisable}
+          handleAddToCart={this.handleAddToCart}
+          buttonText={buttonText}
+        />
+      </Fragment>
     );
   }
 }
@@ -45,5 +81,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  { fetchPendingOrder, removeItemFromCart }
+  { fetchPendingOrder, removeItemFromCart, addItemToCart }
 )(withRouter(OrderSummaryContainer));
