c067b4aa2e5c1c12713c1abd3476220b1ca0f0e4
mndhlovu <mndhlovu@hubspot.com>
2019-10-18 00:58:29 +0100

    added is_paid to donation object

--- cart/models.py
+++ cart/models.py
@@ -71,8 +71,9 @@ class Donation(models.Model):
     stripe_charge_id = models.CharField(max_length=50)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, null=True, blank=True)
-    amount = models.FloatField()
+    price = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
