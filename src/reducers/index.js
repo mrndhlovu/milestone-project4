94ecfccb58b91332fa3e4a5250d596b909192dea
mndhlovu <mndhlovu@hubspot.com>
2019-10-22 00:00:25 +0100

    added article list and article detail to index reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
+import addRemoveCartItemReducer from "./addRemoveCartItemReducer";
+import articleDetailReducer from "./articleDetailReducer";
+import articleListReducer from "./articleListReducer";
 import authReducer from "./authReducer";
 import checkOutReducer from "./checkOutReducer";
 import commentsReducer from "./commentsReducer";
@@ -8,17 +11,17 @@ import editTicketReducer from "./editTicketReducer";
 import errorsReducer from "./errorsReducer";
 import membershipsReducer from "./membershipsReducer";
 import messagesReducer from "./messagesReducer";
+import pendingOrderReducer from "./pendingOrderReducer";
 import ticketDetaiReducer from "./ticketDetailReducer";
+import ticketSolutionReducer from "./ticketSolutionReducer";
 import ticketsReducer from "./ticketsReducer";
 import ticketVoteReducer from "./ticketVoteReducer";
 import userProfileReducer from "./userProfileReducer";
 import userReducer from "./userReducer";
-import addRemoveCartItemReducer from "./addRemoveCartItemReducer";
-import pendingOrderReducer from "./pendingOrderReducer";
-import ticketSolutionReducer from "./ticketSolutionReducer";
-import articleListReducer from "./articleListReducer";
 
 export default combineReducers({
+  articleDetail: articleDetailReducer,
+  articleList: articleListReducer,
   auth: authReducer,
   cartAddOrRemove: addRemoveCartItemReducer,
   checkout: checkOutReducer,
@@ -32,9 +35,8 @@ export default combineReducers({
   ticket: createTicketReducer,
   ticketDetail: ticketDetaiReducer,
   tickets: ticketsReducer,
+  ticketSolution: ticketSolutionReducer,
   user: userReducer,
   userProfile: userProfileReducer,
-  vote: ticketVoteReducer,
-  ticketSolution: ticketSolutionReducer,
-  articleList: articleListReducer
+  vote: ticketVoteReducer
 });
