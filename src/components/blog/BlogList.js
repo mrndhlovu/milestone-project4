bb180bac3b2fa1518ba3e346cf35b083ecc30784
mndhlovu <mndhlovu@hubspot.com>
2019-10-22 00:03:31 +0100

    Update BlogList.js

--- src/components/blog/BlogList.js
+++ src/components/blog/BlogList.js
@@ -1,9 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class BlogList extends Component {
-  render() {
-    return <div>BlogList</div>;
-  }
-}
+import { Grid } from "semantic-ui-react";
+
+import Article from "./Article";
+
+const BlogList = ({ articles }) => {
+  const renderList = () => {
+    return Object.keys(articles).map(key => {
+      const {
+        id,
+        title,
+        created_at,
+        short_disc,
+        likes,
+        views,
+        username
+      } = articles[key];
+      return (
+        <Grid.Column key={key} style={{ paddingBottom: 20 }}>
+          <Article
+            id={id}
+            title={title}
+            created_at={created_at}
+            short_disc={short_disc}
+            likes={likes}
+            views={views}
+            username={username}
+          />
+        </Grid.Column>
+      );
+    });
+  };
+
+  return (
+    <Grid columns={3} stackable>
+      <Grid.Row>{renderList()}</Grid.Row>
+    </Grid>
+  );
+};
 
 export default BlogList;
