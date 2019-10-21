72019637c3929e28e0f2d66e772111444382309a
mndhlovu <mndhlovu@hubspot.com>
2019-10-22 00:03:41 +0100

    Update ArticleDetail.js

--- src/components/blog/ArticleDetail.js
+++ src/components/blog/ArticleDetail.js
@@ -1,7 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-const ArticleDetail = () => {
-  return <div>ArticleDetail</div>;
+import Article from "./Article";
+import { Segment } from "semantic-ui-react";
+import CommentsBody from "../tickets/CommentsBody";
+
+const ArticleDetail = ({ article }) => {
+  const {
+    title,
+    username,
+    short_disc,
+    views,
+    likes,
+    created_at,
+    id,
+    description
+  } = article.data;
+
+  return (
+    <div>
+      <Article
+        id={id}
+        title={title}
+        created_at={created_at}
+        short_disc={short_disc}
+        likes={likes}
+        views={views}
+        username={username}
+        isArticleDetail={true}
+        description={description}
+      />
+      <Segment>
+        <CommentsBody />
+      </Segment>
+    </div>
+  );
 };
 
 export default ArticleDetail;
