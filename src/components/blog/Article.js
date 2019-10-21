5be748ab84595149a07f3058b567ad3ef0d753d5
mndhlovu <mndhlovu@hubspot.com>
2019-10-22 00:04:11 +0100

    Create Article.js

---
+++ src/components/blog/Article.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { Card, Image, Icon } from "semantic-ui-react";
+
+import { getFormatedDate } from "../../utils/appUtils";
+
+const Article = ({
+  title,
+  username,
+  short_disc,
+  views,
+  likes,
+  created_at,
+  id,
+  description,
+  isArticleDetail,
+  handleLikeClick
+}) => {
+  return (
+    <div>
+      <Card fluid>
+        <Image src="/images/avatar/large/daniel.jpg" wrapped ui={false} />
+        <Card.Content>
+          <Card.Header as={Link} to={`/article/${id}`}>
+            {title.toUpperCase()}
+          </Card.Header>
+          <Card.Meta>
+            {username} | {getFormatedDate(created_at)}
+          </Card.Meta>
+          <Card.Description>
+            {isArticleDetail ? description : short_disc}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <span style={{ paddingRight: 20 }}>
+            <Icon
+              link
+              name="thumbs up"
+              color="grey"
+              as="i"
+              fitted
+              onClick={isArticleDetail ? () => handleLikeClick(id) : () => {}}
+            />
+            {likes}
+          </span>
+
+          <Icon name="eye" />
+          {views}
+        </Card.Content>
+      </Card>
+    </div>
+  );
+};
+
+export default Article;
