d0d597ff73f2dcf659e01d5b9ca1dc08498009f3
mndhlovu <mndhlovu@hubspot.com>
2019-10-26 02:33:36 +0100

    handle article or ticket

--- src/components/sharedComponents/EditButtons.js
+++ src/components/sharedComponents/EditButtons.js
@@ -3,18 +3,38 @@ import { NavLink } from "react-router-dom";
 
 import { Button } from "semantic-ui-react";
 
-function EditButtons({ handleTicketDelete, id, isOwner }) {
+function EditButtons({
+  isTicket,
+  handleTicketDelete,
+  handleArticleDelete,
+  id,
+  isOwner
+}) {
   return (
     isOwner && (
       <div style={{ paddingTop: 10 }}>
         <Button.Group floated="right" size="tiny">
-          <Button size="tiny" as={NavLink} to="/create-ticket">
+          <Button
+            size="tiny"
+            as={NavLink}
+            to={isTicket ? "/create-ticket" : "/new-article"}
+          >
             Create
           </Button>
-          <Button as={NavLink} to={`/edit-ticket/${id}`}>
+          <Button
+            as={NavLink}
+            to={`/edit-${isTicket ? "ticket" : "article"}/${id}`}
+          >
             Edit
           </Button>
-          <Button color="blue" onClick={() => handleTicketDelete()}>
+          <Button
+            color="blue"
+            onClick={
+              isTicket
+                ? () => handleTicketDelete()
+                : () => handleArticleDelete()
+            }
+          >
             Delete
           </Button>
         </Button.Group>
