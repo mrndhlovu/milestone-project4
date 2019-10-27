8f7465163bbf790db44d57d55c48c310282164ed
mndhlovu <mndhlovu@hubspot.com>
2019-10-27 13:11:56 +0000

    change formatted data to show time ago

--- src/utils/appUtils.js
+++ src/utils/appUtils.js
@@ -92,20 +92,36 @@ export const getTicketInCart = () => {
   return order;
 };
 
-export const getFormatedDate = rawDate => {
-  const date = new Date(rawDate);
+export const getFormatedDate = lastUpdate => {
+  // From stackoverflow
+  const now = new Date();
+  const previous = new Date(lastUpdate);
 
-  return (
-    date.getFullYear() +
-    "/" +
-    ("0" + (date.getMonth() + 1)).slice(-2) +
-    "/" +
-    ("0" + date.getDate()).slice(-2) +
-    " " +
-    ("0" + date.getHours()).slice(-2) +
-    ":" +
-    ("0" + date.getMinutes()).slice(-2)
-  );
+  const msPerMinute = 60 * 1000;
+  const msPerHour = msPerMinute * 60;
+  const msPerDay = msPerHour * 24;
+  const msPerMonth = msPerDay * 30;
+  const msPerYear = msPerDay * 365;
+
+  const elapsed = now - previous;
+
+  const timeAgo = () => {
+    if (elapsed < msPerMinute) {
+      return `${Math.round(elapsed / 1000)} seconds ago`;
+    } else if (elapsed < msPerHour) {
+      return `${Math.round(elapsed / msPerMinute)} minutes ago`;
+    } else if (elapsed < msPerDay) {
+      return `${Math.round(elapsed / msPerHour)} hours ago`;
+    } else if (elapsed < msPerMonth) {
+      return `${Math.round(elapsed / msPerDay)} days ago`;
+    } else if (elapsed < msPerYear) {
+      return `${Math.round(elapsed / msPerMonth)} months ago`;
+    } else {
+      return `${Math.round(elapsed / msPerYear)} years ago`;
+    }
+  };
+
+  return timeAgo();
 };
 
 export const slugify = text => {
