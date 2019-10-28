981aa855fba768777d90e773516fa74cd9415baf
mndhlovu <mndhlovu@hubspot.com>
2019-10-28 02:58:40 +0000

    get travis to pass by using sqlite db if DATABASE_URL not found

--- unicorn/settings.py
+++ unicorn/settings.py
@@ -87,33 +87,18 @@ WSGI_APPLICATION = 'unicorn.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-DATABASES = {'default': dj_database_url.parse(
-    os.environ.get('DATABASE_URL'))}
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'))}
 
-# if development:
+else:
 
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': os.environ['DATABASE_NAME'],
-#             'USER': os.environ['DATABASE_USER'],
-#             'PASSWORD': os.environ['DATABASE_PASSWORD'],
-#             'HOST': os.environ['DATABASE_HOST'],
-#             'PORT': '5432'
-#         }
-#     }
-
-# else:
-#     if "DATABASE_URL" in os.environ:
-
-#     else:
-#         print("Database URL not found. Using SQLite instead")
-#         DATABASES = {
-#             'default': {
-#                 'ENGINE': 'django.db.backends.sqlite3',
-#                 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#             }
-#         }
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
