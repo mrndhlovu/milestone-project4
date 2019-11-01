e64f819b8eae86e2fe9b04fa6b6afabccaed3d9c
mndhlovu <mndhlovu@hubspot.com>
2019-11-01 01:48:00 +0000

    Update settings.py

--- unicorn/settings.py
+++ unicorn/settings.py
@@ -156,8 +156,10 @@ AWS_STORAGE_BUCKET_NAME = 'unicorn-ecommerce'
 AWS_S3_REGION_NAME = 'eu-west-1'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
