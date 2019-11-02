583a5571ae2030250f64833aef588e2ba9d25b9c
mndhlovu <mndhlovu@hubspot.com>
2019-11-02 03:01:16 +0000

    Format upload image but

--- src/components/userAuth/UserProfileCard.js
+++ src/components/userAuth/UserProfileCard.js
@@ -1,21 +1,24 @@
 import React, { Fragment } from "react";
 
-import { Card, Container, Image, Icon } from "semantic-ui-react";
+import { Card, Container, Image, Icon, Button } from "semantic-ui-react";
 
 import { getFormatedDate } from "../../utils/appUtils";
 
 import { DEFAULT_IMAGE } from "../../constants/constants";
 
 const UserProfileCard = ({ user, handleUploadImage, image }) => {
-  const { username, bio, date_joined } = user;
+  const { username, bio, date_joined, first_name, last_name } = user;
   const defaultImage = DEFAULT_IMAGE === image;
 
   return (
     <Container>
       <Card fluid>
-        <Image src={image} />
+        <Image src={image} wrapped ui={false} />
+
         <Card.Content>
-          <Card.Header>First_name Lastname</Card.Header>
+          <Card.Header>
+            {first_name} {last_name}
+          </Card.Header>
           <Card.Meta>
             <span className="date">Joined {getFormatedDate(date_joined)}</span>
           </Card.Meta>
@@ -24,8 +27,14 @@ const UserProfileCard = ({ user, handleUploadImage, image }) => {
         <Card.Content extra>
           <Icon name="user" color="olive" />
           {username.toUpperCase()}
-          defaultImage && (
-          <input type="file" onChange={event => handleUploadImage(event)} />)
+
+          {defaultImage && (
+            <Button floated="right" color="linkedin">
+              <span style={{ paddingRight: 10 }}> Upload an image</span>
+              <Icon name="upload" color="olive" />
+              <input type="file" onChange={event => handleUploadImage(event)} />
+            </Button>
+          )}
         </Card.Content>
       </Card>
     </Container>
