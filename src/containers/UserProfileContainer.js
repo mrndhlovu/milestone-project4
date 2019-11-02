540a20e68e6d03524cc437b1fdd0afcab7f56b02
mndhlovu <mndhlovu@hubspot.com>
2019-11-02 02:43:02 +0000

    Update profile page adding tab view

--- src/containers/UserProfileContainer.js
+++ src/containers/UserProfileContainer.js
@@ -2,16 +2,22 @@ import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
-import { Confirm, Segment, Container } from "semantic-ui-react";
+import { Confirm, Container, Tab } from "semantic-ui-react";
 
 import { changeAccount } from "../actions/MembershipActions";
 
-import { getUserProfile } from "../selectors/appSelectors";
+import { getUserProfile, getUser } from "../selectors/appSelectors";
 import UserProfileCard from "../components/userAuth/UserProfileCard";
-import { fetchUser, uploadProfileImage } from "../actions/AuthActions";
+import {
+  fetchUser,
+  uploadProfileImage,
+  updateUserProfile
+} from "../actions/AuthActions";
 import PageHeader from "../components/sharedComponents/PageHeader";
 import { getFormatedDate, getFileName } from "../utils/appUtils";
 import { ACCOUNT_CHANGE_OPTION } from "../constants/constants";
+import EditProfile from "../components/userAuth/EditProfile";
+import UserPurchases from "../components/userAuth/UserPurchases";
 
 export class UserProfileContainer extends Component {
   constructor(props) {
@@ -20,29 +26,113 @@ export class UserProfileContainer extends Component {
       showConfirmModal: false,
       userData: "",
       showDeleteAccount: false,
-      option: ""
+      option: "",
+      editFields: ""
     };
 
     this.handleConfirm = this.handleConfirm.bind(this);
     this.handleCancelButtonClick = this.handleCancelButtonClick.bind(this);
     this.handleUploadImage = this.handleUploadImage.bind(this);
+    this.panes = this.panes.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpdateProfile = this.handleUpdateProfile.bind(this);
   }
-  componentDidMount() {
-    this.props.fetchUser();
-  }
+  componentDidMount() {}
 
   componentDidUpdate(prevProps) {
     const { user } = this.props;
-    if (prevProps.user !== user) {
-      if (user.data.username) {
-      }
+
+    if (prevProps.user.dataReceived !== user.dataReceived) {
+      const { first_name, last_name, current_membership: bio } = user.data;
+
+      const editUserFields = { first_name, last_name, ...bio };
+
+      this.setState({ editFields: editUserFields });
     }
+  }
+
+  panes() {
+    const { user } = this.props;
+
+    const allAccess =
+      user.dataReceived &&
+      user.data.current_membership.membership.is_pro_member;
+    const PURCHASES =
+      user.dataReceived && user.data.current_membership.purchases;
+    const accountType = `Unicorn ${allAccess ? "PRO" : "FREE"} membership`;
+
+    return [
+      {
+        menuItem: { key: "user", icon: "user", content: "User profile" },
+        render: () => (
+          <Tab.Pane attached={true}>
+            {user.dataReceived && user.data.username && (
+              <UserProfileCard
+                image={user.data.current_membership.image}
+                user={user.data}
+                handleCancelButtonClick={this.handleCancelButtonClick}
+                handleUploadImage={this.handleUploadImage}
+              />
+            )}
+          </Tab.Pane>
+        )
+      },
+      {
+        menuItem: { key: "edit", icon: "edit", content: "Edit profile" },
+        render: () => (
+          <Tab.Pane attached={false}>
+            {user.dataReceived && (
+              <EditProfile
+                allAccess={allAccess}
+                handleCancelButtonClick={this.handleCancelButtonClick}
+                userData={user.data}
+                handleChange={this.handleChange}
+                handleUpdateProfile={this.handleUpdateProfile}
+              />
+            )}
+          </Tab.Pane>
+        )
+      },
+      {
+        menuItem: {
+          key: "purchases",
+          icon: "shopping basket",
+          content: "Purchases"
+        },
+        render: () => (
+          <Tab.Pane attached={false}>
+            {PURCHASES && user.dataReceived && (
+              <UserPurchases
+                allAccess={allAccess}
+                purchases={PURCHASES}
+                accountType={accountType}
+              />
+            )}
+          </Tab.Pane>
+        )
+      }
+    ];
   }
 
   handleUploadImage(event) {
     const file = event.target.files[0];
-
     this.props.uploadProfileImage(file);
+  }
+
+  handleChange(event, fieldName) {
+    const { editFields } = this.state;
+    event.preventDefault();
+    this.setState({
+      editFields: {
+        ...editFields,
+        [fieldName]: event.target.value
+      }
+    });
+  }
+
+  handleUpdateProfile() {
+    const { editFields } = this.state;
+    this.props.updateUserProfile(editFields);
   }
 
   handleConfirm(option) {
@@ -72,7 +162,7 @@ export class UserProfileContainer extends Component {
     });
   }
   render() {
-    const { user, image } = this.props;
+    const { user } = this.props;
     const { showConfirmModal, option } = this.state;
 
     const headerObject = {
@@ -92,16 +182,10 @@ export class UserProfileContainer extends Component {
           hideButton={true}
         />
         <Container style={{ paddingTop: 20 }}>
-          <Segment>
-            {user.dataReceived && user.data.username && (
-              <UserProfileCard
-                image={user.data.current_membership.image}
-                user={user.data}
-                handleCancelButtonClick={this.handleCancelButtonClick}
-                handleUploadImage={this.handleUploadImage}
-              />
-            )}
-          </Segment>
+          <Tab
+            menu={{ secondary: true, pointing: false }}
+            panes={this.panes()}
+          />
 
           <Confirm
             open={showConfirmModal}
@@ -129,11 +213,12 @@ export class UserProfileContainer extends Component {
 
 const mapStateToProps = state => {
   return {
-    user: getUserProfile(state)
+    user: getUserProfile(state),
+    auth: getUser(state)
   };
 };
 
 export default connect(
   mapStateToProps,
-  { changeAccount, fetchUser, uploadProfileImage }
+  { changeAccount, fetchUser, uploadProfileImage, updateUserProfile }
 )(withRouter(UserProfileContainer));
