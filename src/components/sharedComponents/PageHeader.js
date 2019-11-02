880efb6dcb87b7fcb0f5a780c5feaa7951def270
mndhlovu <mndhlovu@hubspot.com>
2019-11-02 02:44:38 +0000

    Update PageHeader.js remove div wrap

--- src/components/sharedComponents/PageHeader.js
+++ src/components/sharedComponents/PageHeader.js
@@ -22,47 +22,46 @@ export const PageHeader = ({
     headerText,
     headerButtonText,
     headerButtonUrl,
-    subHeading
+    subHeading,
+    image
   } = customHeader ? headerObject : getHeaderObject();
 
   return (
-    <div>
-      <StyledSegment textAlign="center" inverted vertical>
-        <Header
-          as="h1"
-          content={headerText}
-          inverted
-          style={{
-            fontSize: mobile ? "2em" : "3em",
-            fontWeight: "normal",
-            marginBottom: 0,
-            marginTop: mobile ? "1em" : "1em"
-          }}
-        />
-        <Header
-          as="h2"
-          content={subHeading}
-          inverted
-          style={{
-            fontSize: mobile ? "1.5em" : "1.7em",
-            fontWeight: "normal",
-            marginTop: mobile ? "0.5em" : "1.5em"
-          }}
-        />
-        {!hideButton && (
-          <Button
-            primary
-            color="linkedin"
-            size="large"
-            as={NavLink}
-            to={headerButtonUrl}
-          >
-            {headerButtonText}
-            <Icon name="right arrow" />
-          </Button>
-        )}
-      </StyledSegment>
-    </div>
+    <StyledSegment textAlign="center" inverted vertical>
+      <Header
+        as="h1"
+        content={headerText}
+        inverted
+        style={{
+          fontSize: mobile ? "2em" : "3em",
+          fontWeight: "normal",
+          marginBottom: 0,
+          marginTop: mobile ? "1em" : "1em"
+        }}
+      />
+      <Header
+        as="h2"
+        content={subHeading}
+        inverted
+        style={{
+          fontSize: mobile ? "1.5em" : "1.7em",
+          fontWeight: "normal",
+          marginTop: mobile ? "0.5em" : "1.5em"
+        }}
+      />
+      {!hideButton && (
+        <Button
+          primary
+          color="linkedin"
+          size="large"
+          as={NavLink}
+          to={headerButtonUrl}
+        >
+          {headerButtonText}
+          <Icon name="right arrow" />
+        </Button>
+      )}
+    </StyledSegment>
   );
 };
 
