aa785e2a7e08c6abab358d47481bbdf00aaa3013
mndhlovu <mndhlovu@hubspot.com>
2019-11-03 13:00:00 +0000

    Update DynamicHeader.js

--- src/components/sharedComponents/DynamicHeader.js
+++ src/components/sharedComponents/DynamicHeader.js
@@ -1,60 +1,27 @@
 import React from "react";
-import { getDynamicHeader } from "../../constants/headerConstants";
-
-import { NavLink } from "react-router-dom";
 
 import styled from "styled-components";
 
-import { Button, Header, Icon, Segment } from "semantic-ui-react";
+import { Header, Segment } from "semantic-ui-react";
 
 const StyledSegment = styled(Segment)`
-  padding-bottom: 5rem !important;
+  background-image: url(${props => props.image}) !important;
+  height: 35vh;
 `;
 
-const DynamicHeader = ({ option, mobile }) => {
-  const {
-    headerText,
-    headerButtonText,
-    headerButtonUrl,
-    subHeading
-  } = getDynamicHeader(option);
+const StyledHeader = styled(Header)`
+  font-size: 3em !important;
+  font-weight: 100 !important;
+  padding-top: 4rem !important;
+`;
 
+const DynamicHeader = ({ image, articleTitle }) => {
   return (
-    <div>
-      <StyledSegment textAlign="center" inverted vertical>
-        <Header
-          as="h1"
-          content={headerText}
-          inverted
-          style={{
-            fontSize: mobile ? "2em" : "3em",
-            fontWeight: "normal",
-            marginBottom: 0,
-            marginTop: mobile ? "1em" : "1em"
-          }}
-        />
-        <Header
-          as="h2"
-          content={subHeading}
-          inverted
-          style={{
-            fontSize: mobile ? "1.5em" : "1.7em",
-            fontWeight: "normal",
-            marginTop: mobile ? "0.5em" : "1.5em"
-          }}
-        />
-        <Button
-          primary
-          color="linkedin"
-          size="large"
-          as={NavLink}
-          to={headerButtonUrl}
-        >
-          {headerButtonText}
-          <Icon name="right arrow" />
-        </Button>
-      </StyledSegment>
-    </div>
+    <StyledSegment textAlign="center" inverted vertical image={image}>
+      <StyledHeader as="h1" inverted>
+        {articleTitle.toUpperCase()}
+      </StyledHeader>
+    </StyledSegment>
   );
 };
 
