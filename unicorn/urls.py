6ec426b7145d2b10d08f334a4e49fff2ac87029c
mndhlovu <mndhlovu@hubspot.com>
2019-11-13 01:03:47 +0000

    Update urls.py

--- unicorn/urls.py
+++ unicorn/urls.py
@@ -15,12 +15,4 @@ urlpatterns = [
     path('blog/', include('blog.api.urls')),
 
     re_path('.*', TemplateView.as_view(template_name='index.html')),
-
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
