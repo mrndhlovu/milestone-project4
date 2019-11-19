392b011dd8a5aff3deec014eb6826ced332e3a5f
mndhlovu <mndhlovu@hubspot.com>
2019-11-19 23:40:21 +0000

    Update TicketsList.js

--- src/components/tickets/TicketsList.js
+++ src/components/tickets/TicketsList.js
@@ -1,17 +1,42 @@
 import React from "react";
 
-import DashboardCard from "../sharedComponents/DashboardCard";
+import StyledMessage from "../sharedComponents/StyledMessage";
+import Tickets from "./Tickets";
+import { getObjectLength } from "../../utils/appUtils";
 
-const TicketsList = ({ component, ticketCount, isLoading, size }) => {
+import { Card, Segment } from "semantic-ui-react";
+
+const TicketsList = ({
+  tickets,
+  isLoading,
+  user,
+  buttonText,
+  handleAddToCart
+}) => {
+  const ticketCount = getObjectLength(tickets);
+
   return (
-    <DashboardCard
-      header="TICKETS"
-      headerSize="h3"
-      subheader={`TOTAL:  ${ticketCount}`}
-      component={component}
-      isLoading={isLoading}
-      size={size}
-    />
+    <Card fluid>
+      <Card.Content>
+        <Card.Header as="h3" content={`${ticketCount} TICKETS`} />
+        <Segment attached stacked loading={isLoading}>
+          {tickets.length > 0 ? (
+            <Tickets
+              handleAddToCart={handleAddToCart}
+              ticketsList={tickets}
+              user={user}
+              buttonText={buttonText}
+            />
+          ) : (
+            <StyledMessage
+              message="No tickets at this time."
+              redirect="/create-ticket"
+              linkText="Create a ticket"
+            />
+          )}
+        </Segment>
+      </Card.Content>
+    </Card>
   );
 };
 
