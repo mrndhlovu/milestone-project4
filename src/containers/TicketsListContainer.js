ed4bd6d387e2f94e49813478172d1572796e6d8b
mndhlovu <mndhlovu@hubspot.com>
2019-11-19 23:40:11 +0000

    clean up and removed ticket voting from list

--- src/containers/TicketsListContainer.js
+++ src/containers/TicketsListContainer.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
 
-import { fetchTicketsList, updatedTicketVote } from "../actions/TicketActions";
+import { fetchTicketsList } from "../actions/TicketActions";
 import { addItemToCart, fetchPendingOrder } from "../actions/CheckoutActions";
 import PageHeader from "../components/sharedComponents/PageHeader";
 import {
@@ -11,10 +11,7 @@ import {
   getUserProfile,
   getUser
 } from "../selectors/appSelectors";
-import Tickets from "../components/tickets/Tickets";
-import { getObjectLength, getTicketTotals } from "../utils/appUtils";
 import TicketsList from "../components/tickets/TicketsList";
-import StyledMessage from "../components/sharedComponents/StyledMessage";
 import { Grid, Container } from "semantic-ui-react";
 import { APP_TYPE } from "../constants/constants";
 import ListSideBar from "../components/tickets/ListSideBar";
@@ -29,30 +26,13 @@ export class TicketsListContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tickets: "",
       buttonText: "Add to cart"
     };
     this.handleAddToCart = this.handleAddToCart.bind(this);
-    this.handleVote = this.handleVote.bind(this);
   }
 
   componentDidMount() {
-    const { ticketsList } = this.props;
     this.props.fetchTicketsList();
-    if (ticketsList.dataReceived) {
-      this.setState({ tickets: ticketsList.data });
-    }
-  }
-
-  handleVote(id) {
-    const { user } = this.props;
-    const allAccess =
-      user.dataReceived &&
-      user.data.current_membership.membership.is_pro_member;
-
-    if (allAccess) {
-      this.props.updatedTicketVote(id);
-    }
   }
 
   handleAddToCart(id) {
@@ -64,63 +44,33 @@ export class TicketsListContainer extends Component {
     }
   }
 
-  componentDidUpdate(prevProps) {
-    const { ticketsList } = this.props;
-
-    if (prevProps.ticketsList !== ticketsList) {
-      if (ticketsList.dataReceived) {
-        this.setState({ tickets: ticketsList.data });
-      }
-    }
-  }
-
   render() {
     const { ticketsList, user } = this.props;
-    const { tickets, buttonText } = this.state;
-    const ticketCount = getObjectLength(tickets);
-
-    const { bugCount, featureCount, closed } = getTicketTotals(tickets);
+    const { buttonText } = this.state;
 
     return (
-      <Fragment>
-        <PageHeader pageId={getPageId()} />
-        <SytledContainer fluid>
-          <Grid stackable columns={2}>
-            <Grid.Column width={4}>
-              <ListSideBar
-                total={ticketCount}
-                closed={closed}
-                featureCount={featureCount}
-                bugCount={bugCount}
-              />
-            </Grid.Column>
+      ticketsList.dataReceived && (
+        <Fragment>
+          <PageHeader pageId={getPageId()} dataTestId="tickets-page-header" />
+          <SytledContainer fluid>
+            <Grid stackable columns={2}>
+              <Grid.Column width={4}>
+                <ListSideBar data={ticketsList.data} />
+              </Grid.Column>
 
-            <Grid.Column width={12}>
-              <TicketsList
-                ticketCount={ticketCount}
-                isLoading={tickets.isLoading}
-                component={
-                  ticketsList.data.length > 0 && ticketsList.dataReceived ? (
-                    <Tickets
-                      handleAddToCart={this.handleAddToCart}
-                      handleVote={this.handleVote}
-                      ticketsList={tickets}
-                      user={user}
-                      buttonText={buttonText}
-                    />
-                  ) : (
-                    <StyledMessage
-                      message="No tickets at this time."
-                      redirect="/create-ticket"
-                      linkText="Create a ticket"
-                    />
-                  )
-                }
-              />
-            </Grid.Column>
-          </Grid>
-        </SytledContainer>
-      </Fragment>
+              <Grid.Column width={12}>
+                <TicketsList
+                  tickets={ticketsList.data}
+                  isLoading={ticketsList.isLoading}
+                  buttonText={buttonText}
+                  user={user}
+                  handleAddToCart={this.handleAddToCart}
+                />
+              </Grid.Column>
+            </Grid>
+          </SytledContainer>
+        </Fragment>
+      )
     );
   }
 }
@@ -137,6 +87,5 @@ const mapStateToProps = state => {
 export default connect(mapStateToProps, {
   fetchTicketsList,
   addItemToCart,
-  fetchPendingOrder,
-  updatedTicketVote
+  fetchPendingOrder
 })(TicketsListContainer);
