b51eaeff6469ab70e26f3132db2e64aa5f6fe142
mndhlovu <mndhlovu@hubspot.com>
2019-11-19 01:40:05 +0000

    Update WelcomePage.test.js

--- spec/components/WelcomePage.test.js
+++ spec/components/WelcomePage.test.js
@@ -7,21 +7,12 @@ import PageHeader from "../../src/components/sharedComponents/PageHeader";
 import { Header, List } from "semantic-ui-react";
 import FooterLinks from "../../src/components/navigation/FooterLinks";
 import NavFooter from "../../src/components/navigation/NavFooter";
+import { mockWindowObject, findByDataTestId } from "../testUtils.js/utils";
 
 const headerProps = { ...HEADER_TEXT.home };
 const DEFAULT_PROPS = {
   pageId: "home",
   buttonId: "get-started-button"
-};
-
-const mockWindowObject = pathname => {
-  global.window = Object.create(window);
-
-  Object.defineProperty(window, "location", {
-    value: {
-      pathname
-    }
-  });
 };
 
 describe("HomeContainer", () => {
@@ -32,7 +23,7 @@ describe("HomeContainer", () => {
   let container;
 
   it("Mounts correctly", () => {
-    container = wrapper.find('[data-test-id="welcome-page"]').first();
+    container = findByDataTestId(wrapper, "welcome-page").first();
     expect(container).toBeDefined();
   });
 
@@ -46,9 +37,7 @@ describe("HomeContainer", () => {
 
   it("Should click on get started button and redirect to pricing page", () => {
     mockWindowObject("/pricing");
-    const getStartedButton = header.find(
-      `[data-test-id="${DEFAULT_PROPS.buttonId}"]`
-    );
+    const getStartedButton = findByDataTestId(header, DEFAULT_PROPS.buttonId);
 
     getStartedButton.simulate("click");
     expect(window.location.pathname).toMatch("/pricing");
@@ -56,31 +45,29 @@ describe("HomeContainer", () => {
 
   it("Should click on all access button and redirect to pricing page", () => {
     mockWindowObject("/pricing");
-    const allAccessButton = wrapper.find('[data-test-id="all-access-button"]');
-    allAccessButton.simulate("click");
+    const allAccessButton = findByDataTestId(wrapper, "all-access-button");
 
+    allAccessButton.simulate("click");
     expect(window.location.pathname).toMatch("/pricing");
   });
 
   it("Should click on donation button and redirect to login page", () => {
     mockWindowObject("/login");
-    let donationDonationButton = wrapper.find(
-      '[data-test-id="make-donation-button"]'
-    );
+    let donationButton = findByDataTestId(wrapper, "make-donation-button");
 
-    donationDonationButton.simulate("click");
+    donationButton.simulate("click");
     expect(window.location.pathname).toMatch("/login");
   });
 
   it("Should render footer and footer links", () => {
-    let container = footer.find('[data-test-id="footer-links"]');
+    let container = findByDataTestId(footer, "footer-links");
     const links = container.find(List.Item);
 
     expect(links.length).toBe(5);
   });
 
   it("Should render  footer icons", () => {
-    let container = footerIcons.find('[data-test-id="footer-icons"]');
+    let container = findByDataTestId(footerIcons, "footer-icons");
     const links = container.find(List.Icon);
 
     expect(links.length).toBe(2);
