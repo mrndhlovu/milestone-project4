4d5e98ffaf2c6a77a6a39513bfd3ac2db3cb36a0
mndhlovu <mndhlovu@hubspot.com>
2019-11-19 01:40:41 +0000

    Update MembershipServiceList.js

--- src/containers/MembershipServiceList.js
+++ src/containers/MembershipServiceList.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import { List, Icon } from "semantic-ui-react";
 
-const MembershipServiceList = ({ services }) => {
+const MembershipServiceList = ({ services, listId }) => {
   return services.map((service, index) => {
     return (
       <List style={{ paddingLeft: 20 }} key={index}>
         <List.Item>
           <Icon name="check" />
-          <List.Content>
+          <List.Content data-test-id={listId}>
             <List.Description>{service}</List.Description>
           </List.Content>
         </List.Item>
