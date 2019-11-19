754393a62274893ec19e21777d366b9b87098cbd
mndhlovu <mndhlovu@hubspot.com>
2019-11-19 23:45:46 +0000

    Update TicketsList.js

--- src/components/tickets/TicketsList.js
+++ src/components/tickets/TicketsList.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
+import styled from "styled-components";
 
 import StyledMessage from "../sharedComponents/StyledMessage";
 import Tickets from "./Tickets";
 import { getObjectLength } from "../../utils/appUtils";
 
-import { Card, Segment } from "semantic-ui-react";
+import { Card, Segment, Container, Grid } from "semantic-ui-react";
+import PageHeader from "../sharedComponents/PageHeader";
+import { getPageId } from "../../utils/urls";
+import ListSideBar from "./ListSideBar";
+
+const SytledContainer = styled(Container)`
+  padding: 20px 5px;
+  background-color: #eee;
+`;
 
 const TicketsList = ({
   tickets,
@@ -16,27 +25,41 @@ const TicketsList = ({
   const ticketCount = getObjectLength(tickets);
 
   return (
-    <Card fluid>
-      <Card.Content>
-        <Card.Header as="h3" content={`${ticketCount} TICKETS`} />
-        <Segment attached stacked loading={isLoading}>
-          {tickets.length > 0 ? (
-            <Tickets
-              handleAddToCart={handleAddToCart}
-              ticketsList={tickets}
-              user={user}
-              buttonText={buttonText}
-            />
-          ) : (
-            <StyledMessage
-              message="No tickets at this time."
-              redirect="/create-ticket"
-              linkText="Create a ticket"
-            />
-          )}
-        </Segment>
-      </Card.Content>
-    </Card>
+    <Fragment>
+      <PageHeader pageId={getPageId()} dataTestId="tickets-page-header" />
+
+      <SytledContainer fluid>
+        <Grid stackable columns={2}>
+          <Grid.Column width={4}>
+            <ListSideBar data={tickets} />
+          </Grid.Column>
+
+          <Grid.Column width={12}>
+            <Card fluid>
+              <Card.Content>
+                <Card.Header as="h3" content={`${ticketCount} TICKETS`} />
+                <Segment attached stacked loading={isLoading}>
+                  {tickets.length > 0 ? (
+                    <Tickets
+                      handleAddToCart={handleAddToCart}
+                      ticketsList={tickets}
+                      user={user}
+                      buttonText={buttonText}
+                    />
+                  ) : (
+                    <StyledMessage
+                      message="No tickets at this time."
+                      redirect="/create-ticket"
+                      linkText="Create a ticket"
+                    />
+                  )}
+                </Segment>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </Grid>
+      </SytledContainer>
+    </Fragment>
   );
 };
 
