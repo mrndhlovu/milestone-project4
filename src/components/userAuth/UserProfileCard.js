742eda45edcd18145620e7f7e45fac257f0df9e1
mndhlovu <mndhlovu@hubspot.com>
2019-11-26 01:41:57 +0000

    Update UserProfileCard.js

--- src/components/userAuth/UserProfileCard.js
+++ src/components/userAuth/UserProfileCard.js
@@ -1,17 +1,11 @@
-import React, { Fragment } from "react";
+import React from "react";
 
-import { Card, Image, Icon } from "semantic-ui-react";
+import { Card, Image } from "semantic-ui-react";
 
 import { getFormatedDate } from "../../utils/appUtils";
 import { DEFAULT_IMAGES } from "../../constants/constants";
-import UploadImageButton from "../sharedComponents/UploadImageButton";
 
-const UserProfileCard = ({
-  user,
-  handleUploadImage,
-  image,
-  handleEditImage
-}) => {
+const UserProfileCard = ({ user, image, handleEditImage }) => {
   const {
     username,
     date_joined,
@@ -43,18 +37,6 @@ const UserProfileCard = ({
           <span className="date">Joined {getFormatedDate(date_joined)}</span>
         </Card.Meta>
         {occupation && <Card.Description>{occupation}</Card.Description>}
-      </Card.Content>
-      <Card.Content extra>
-        {isUsingDefaultImage && (
-          <Fragment>
-            <Icon name="image" /> Upload image
-            <input
-              type="file"
-              onChange={event => handleUploadImage(event)}
-              data-test-id="upload-image-input-button"
-            />
-          </Fragment>
-        )}
       </Card.Content>
     </Card>
   );
