8c586c4eb58bad6a65ee6965a0b1298f1c0b04c4
mndhlovu <mndhlovu@hubspot.com>
2019-12-05 19:31:06 +0000

    Update StyledMessage.js

--- src/components/sharedComponents/StyledMessage.js
+++ src/components/sharedComponents/StyledMessage.js
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom";
 
 import { Header, Icon, Segment, Button } from "semantic-ui-react";
 
-const StyledMessage = ({ redirect, linkText, message }) => {
+const StyledMessage = ({ redirect, linkText, message, iconName }) => {
   return (
     <Segment placeholder>
       <Header icon>
-        <Icon name="list alternate outline" />
+        <Icon name={iconName} />
         {message}
       </Header>
 
