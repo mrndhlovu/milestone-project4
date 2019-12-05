cef6f4fddb13e9fc0a279b01354e17d03da85244
mndhlovu <mndhlovu@hubspot.com>
2019-12-05 20:32:06 +0000

    Update OrderSummary.js

--- src/components/ecommerce/OrderSummary.js
+++ src/components/ecommerce/OrderSummary.js
@@ -2,21 +2,13 @@ import React from "react";
 
 import { Container } from "semantic-ui-react";
 
-import UILoadingSpinner from "../sharedComponents/UILoadingSpinner";
 import CartFooter from "./CartFooter";
-
 import CartBody from "./CartBody";
 
-const OrderSummary = ({
-  pendingOrders,
-  handleRemoveClick,
-  history,
-  total,
-  cartIsEmpty
-}) => {
+const OrderSummary = ({ pendingOrders, handleRemoveClick, history, total }) => {
   const orderItems = pendingOrders.orders ? pendingOrders.orders : {};
 
-  return !cartIsEmpty ? (
+  return (
     <Container>
       <CartBody
         orderItems={orderItems}
@@ -27,8 +19,6 @@ const OrderSummary = ({
 
       <CartFooter total={pendingOrders.total} />
     </Container>
-  ) : (
-    <UILoadingSpinner />
   );
 };
 
