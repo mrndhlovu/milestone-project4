d4fdfe173d4879aac964e4f15a84751a3d3fd9c5
mndhlovu <mndhlovu@hubspot.com>
2019-12-05 19:25:46 +0000

    Update CardPaymentForm.js

--- src/components/ecommerce/CardPaymentForm.js
+++ src/components/ecommerce/CardPaymentForm.js
@@ -1,37 +1,86 @@
 import React from "react";
+import styled from "styled-components";
 
-import { Form, Card, Segment, Button } from "semantic-ui-react";
+import {
+  Form,
+  Card,
+  Segment,
+  Button,
+  Container,
+  Accordion,
+  Icon
+} from "semantic-ui-react";
 
 import { CardElement } from "react-stripe-elements";
+import BillingDetails from "./BillingDetails";
+
+const StyledContainer = styled(Container)`
+  padding-top: 10px;
+`;
+
+const StyledSpan = styled(Container)`
+  font-size: 18px;
+`;
 
 const CardPaymentForm = ({
   handleOnFocus,
   handlePayNow,
   isDisabled,
-  isLoading
+  isLoading,
+  handleAccordionClick,
+  activeIndex,
+  handleChange
 }) => {
   return (
-    <Card fluid style={{ borderRadius: 0 }}>
-      <Card.Content header="Payment" />
-      <Card.Content>
-        <Form.Group widths="equal">
-          <Segment>
-            <CardElement onFocus={() => handleOnFocus()} />
-          </Segment>
-        </Form.Group>
-      </Card.Content>
-      <Card.Content extra>
-        <Button
-          color="orange"
-          fluid
-          onClick={() => handlePayNow()}
-          loading={isLoading}
-          disabled={isDisabled}
-        >
-          PAY NOW
-        </Button>
-      </Card.Content>
-    </Card>
+    <StyledContainer style={{ borderRadius: 0 }}>
+      <Segment>
+        <Accordion>
+          <Accordion.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={() => handleAccordionClick(0)}
+          >
+            <StyledSpan>
+              <Icon name="dropdown" />
+              Personal Details
+            </StyledSpan>
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            <BillingDetails handleChange={handleChange} />
+          </Accordion.Content>
+          <Accordion.Title
+            active={activeIndex === 1}
+            index={1}
+            onClick={() => handleAccordionClick(1)}
+          >
+            <StyledSpan>
+              <Icon name="dropdown" />
+              Payment
+            </StyledSpan>
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1}>
+            <Card.Content>
+              <Form.Group widths="equal">
+                <Segment>
+                  <CardElement onFocus={() => handleOnFocus()} />
+                </Segment>
+              </Form.Group>
+            </Card.Content>
+            <Card.Content extra>
+              <Button
+                color="orange"
+                fluid
+                onClick={() => handlePayNow()}
+                loading={isLoading}
+                disabled={isDisabled}
+              >
+                PAY NOW
+              </Button>
+            </Card.Content>
+          </Accordion.Content>
+        </Accordion>
+      </Segment>
+    </StyledContainer>
   );
 };
 
