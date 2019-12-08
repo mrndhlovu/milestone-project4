ec76f8cdedc1ba3e4cbf5ec2efb59cf6cfd3f076
mndhlovu <mndhlovu@hubspot.com>
2019-12-08 16:44:10 +0000

    Update CheckoutContainer.js

--- src/containers/CheckoutContainer.js
+++ src/containers/CheckoutContainer.js
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { injectStripe, Elements, StripeProvider } from "react-stripe-elements";
 
 import PaymentContainer from "./PaymentContainer";
 
-class CheckoutContainer extends Component {
-  render() {
-    const { stripe } = this.props;
-    return <PaymentContainer stripe={stripe} />;
-  }
-}
+const CheckoutContainer = props => {
+  const { stripe } = props;
+  return <PaymentContainer stripe={stripe} />;
+};
 
 const FormInjection = injectStripe(CheckoutContainer);
 
